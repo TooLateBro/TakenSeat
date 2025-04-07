606c9dadae40b4a2866aacda9b4480678ba23e6d
JUNG DAYE <jungyj0809@gmail.com>
2025-04-08 04:38:26 +0900

    feat: #41 공연 엔티티 정의 및 공연 상태, 회차 연관관계 설정
    - 공연 기본 정보 정의
    - 공연 상태(PerformanceStatus) Enum 사용
    - 공연 회차와 OneToMany 관계 설정

---
+++ com.taken_seat.performance-service/src/main/java/com/taken_seat/performance_service/performance/domain/model/Performance.java
@@ -0,0 +1,63 @@
+package com.taken_seat.performance_service.performance.domain.model;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "p_performances")
+@Entity
+public class Performance {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	@Column(columnDefinition = "BINARY(16)", updatable = false, nullable = false)
+	private UUID id;
+
+	@Column(nullable = false, length = 100)
+	private String title;
+
+	@Column(nullable = false)
+	private String description;
+
+	@Column(nullable = false)
+	private LocalDateTime startAt;
+
+	@Column(nullable = false)
+	private LocalDateTime endAt;
+
+	@Column(nullable = false)
+	@Enumerated(EnumType.STRING)
+	private PerformanceStatus status;
+
+	private String posterUrl;
+
+	@Column(length = 100)
+	private String ageLimit;
+
+	private Integer maxTicketCount;
+
+	private String discountInfo;
+
+	@OneToMany(mappedBy = "performance", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<PerformanceSchedule> schedules = new ArrayList<>();
+}
