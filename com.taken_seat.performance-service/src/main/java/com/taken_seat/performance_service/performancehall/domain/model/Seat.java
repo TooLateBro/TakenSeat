209964e3367963748841de37f95a5abfec19b416
JUNG DAYE <jungyj0809@gmail.com>
2025-04-08 04:42:15 +0900

    feat: #41 좌석 엔티티 정의
    - 공연장 ID 연관관계 설정

---
+++ com.taken_seat.performance-service/src/main/java/com/taken_seat/performance_service/performancehall/domain/model/Seat.java
@@ -0,0 +1,51 @@
+package com.taken_seat.performance_service.performancehall.domain.model;
+
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "p_seats")
+@Entity
+public class Seat {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	@Column(columnDefinition = "BINARY(16", updatable = false, nullable = false)
+	private UUID id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "performance_hall_id", nullable = false)
+	private PerformanceHall performanceHall;
+
+	@Column(nullable = false, length = 10)
+	private String rowNumber;
+
+	@Column(nullable = false, length = 10)
+	private String seatNumber;
+
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private SeatType seatType;
+
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private SeatStatus seatStatus;
+}
