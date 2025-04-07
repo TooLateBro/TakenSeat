fd0315014cb9f3b2997ab6415c8c7879e8c2e78c
JUNG DAYE <jungyj0809@gmail.com>
2025-04-08 04:40:27 +0900

    feat: #41 공연 회차 엔티티 정의
    - 공연 회차 정보 정의
    - 공연 회차 상태 Enum 사용
    - 공연 ID 연관관계 ManyToOne 매핑
    - 회차별 좌석 가격과 OneToMany 관계 설정

---
+++ com.taken_seat.performance-service/src/main/java/com/taken_seat/performance_service/performance/domain/model/PerformanceSchedule.java
@@ -0,0 +1,57 @@
+package com.taken_seat.performance_service.performance.domain.model;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "p_performance_schedules")
+@Entity
+public class PerformanceSchedule {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	@Column(columnDefinition = "BINARY(16)", updatable = false, nullable = false)
+	private UUID id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "performance_id", nullable = false)
+	private Performance performance;
+
+	@Column(name = "performance_hall_id", columnDefinition = "BINARY(16)", nullable = false)
+	private UUID performanceHallId;
+
+	@Column(nullable = false)
+	private LocalDateTime saleStartAt;
+
+	private LocalDateTime saleEndAt;
+
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private PerformanceScheduleStatus status;
+
+	@OneToMany(mappedBy = "performanceSchedule", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<PerformanceSeatPrice> seatPrices = new ArrayList<>();
+}
