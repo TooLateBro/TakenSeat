e5c7c0c5248d7ca97c8fc451ff16448d22204642
JUNG DAYE <jungyj0809@gmail.com>
2025-04-08 04:41:35 +0900

    feat: #41 공연 회차별 좌석 가격 엔티티 정의
    - 공연 회차 연관관계 설정

---
+++ com.taken_seat.performance-service/src/main/java/com/taken_seat/performance_service/performance/domain/model/PerformanceSeatPrice.java
@@ -0,0 +1,46 @@
+package com.taken_seat.performance_service.performance.domain.model;
+
+import java.util.UUID;
+
+import com.taken_seat.performance_service.performancehall.domain.model.SeatType;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "p_performance_seat_prices")
+@Entity
+public class PerformanceSeatPrice {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	@Column(columnDefinition = "BINARY(16)", updatable = false, nullable = false)
+	private UUID id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "performance_schedule_id", nullable = false)
+	private PerformanceSchedule performanceSchedule;
+
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private SeatType seatType;
+
+	@Column(nullable = false)
+	private Integer price;
+}
