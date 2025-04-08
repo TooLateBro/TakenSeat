1081ab77f04be9a57db8f10130de86786838d1f7
JUNG DAYE <jungyj0809@gmail.com>
2025-04-09 01:56:58 +0900

    feat: #30 공연 상세 응답 매핑 로직 추가
    - Performance → DetailResponseDto 변환 메서드 추가

--- com.taken_seat.performance-service/src/main/java/com/taken_seat/performance_service/performance/application/dto/mapper/ResponseMapper.java
+++ com.taken_seat.performance-service/src/main/java/com/taken_seat/performance_service/performance/application/dto/mapper/ResponseMapper.java
@@ -3,6 +3,7 @@ package com.taken_seat.performance_service.performance.application.dto.mapper;
 import java.util.stream.Collectors;
 
 import com.taken_seat.performance_service.performance.application.dto.response.CreateResponseDto;
+import com.taken_seat.performance_service.performance.application.dto.response.DetailResponseDto;
 import com.taken_seat.performance_service.performance.application.dto.response.PerformanceScheduleResponseDto;
 import com.taken_seat.performance_service.performance.application.dto.response.SeatPriceResponseDto;
 import com.taken_seat.performance_service.performance.domain.model.Performance;
@@ -50,9 +51,29 @@ public class ResponseMapper {
 
 	private static SeatPriceResponseDto toSeatPriceDto(PerformanceSeatPrice seatPrice) {
 		return SeatPriceResponseDto.builder()
-			.PerformanceSeatPriceId(seatPrice.getId())
+			.performanceSeatPriceId(seatPrice.getId())
 			.seatType(seatPrice.getSeatType())
 			.price(seatPrice.getPrice())
+			.build();
+	}
+
+	public static DetailResponseDto detailToDto(Performance performance) {
+		return DetailResponseDto.builder()
+			.performanceId(performance.getId())
+			.title(performance.getTitle())
+			.description(performance.getDescription())
+			.startAt(performance.getStartAt())
+			.endAt(performance.getEndAt())
+			.status(performance.getStatus())
+			.posterUrl(performance.getPosterUrl())
+			.ageLimit(performance.getAgeLimit())
+			.maxTicketCount(performance.getMaxTicketCount())
+			.discountInfo(performance.getDiscountInfo())
+			.schedules(
+				performance.getSchedules().stream()
+					.map(ResponseMapper::toScheduleDto)
+					.collect(Collectors.toList())
+			)
 			.build();
 	}
 }
