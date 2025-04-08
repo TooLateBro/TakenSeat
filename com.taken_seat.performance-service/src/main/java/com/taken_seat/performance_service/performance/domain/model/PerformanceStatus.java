e8765f557b27b2af46596d495660847a75b743b6
JUNG DAYE <jungyj0809@gmail.com>
2025-04-08 18:26:51 +0900

    feat: #29 공연 상태 분기 로직 구현
    - 공연 시작/종료일 기준으로 UPCOMING, LIVE, CLOSED 분기 로직 구현

--- com.taken_seat.performance-service/src/main/java/com/taken_seat/performance_service/performance/domain/model/PerformanceStatus.java
+++ com.taken_seat.performance-service/src/main/java/com/taken_seat/performance_service/performance/domain/model/PerformanceStatus.java
@@ -1,8 +1,25 @@
 package com.taken_seat.performance_service.performance.domain.model;
 
+import java.time.LocalDateTime;
+
 public enum PerformanceStatus {
 
 	UPCOMING,
 	LIVE,
-	CLOSED
+	CLOSED;
+
+	public static PerformanceStatus status(LocalDateTime startAt, LocalDateTime endAt) {
+
+		LocalDateTime now = LocalDateTime.now();
+
+		if (now.isBefore(startAt)) {
+			return UPCOMING;
+		}
+
+		if ((now.isEqual(startAt) || now.isAfter(startAt)) && !now.isAfter(endAt)) {
+			return LIVE;
+		}
+
+		return CLOSED;
+	}
 }
