4ab95909b5b92fe79bf41660e09c120e8facf199
SeungGyu <seungg8361@naver.com>
2025-04-09 01:40:14 +0900

    feat : #62 Auth 유저 정보 수정
    - 수정하고 싶은 필드만 수정할 수 있습니다.

---
+++ com.taken_seat.auth-service/src/main/java/com/taken_seat/auth_service/presentation/dto/user/UserUpdateRequestDto.java
@@ -0,0 +1,44 @@
+package com.taken_seat.auth_service.presentation.dto.user;
+
+import com.taken_seat.auth_service.application.dto.user.UserUpdateDto;
+import com.taken_seat.auth_service.domain.vo.Role;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserUpdateRequestDto {
+
+    @Size(min = 4, max = 10, message = "이름은 최소 4자 이상, 10자 이하이어야 합니다.")
+    @Pattern(regexp = "^[a-z0-9]+$", message = "이름은 알파벳 소문자(a~z)와 숫자(0~9)로만 구성되어야 합니다.")
+    private String username = null;
+
+    @Size(min = 8, max = 15, message = "비밀번호는 최소 8자 이상, 15자 이하이어야 합니다.")
+    @Pattern(
+            regexp = "^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+=-]).+$",
+            message = "비밀번호는 알파벳 대소문자, 숫자, 특수문자를 모두 포함해야 합니다."
+    )
+    private String password = null;
+
+    @Email(message = "올바른 이메일 형식이 아닙니다.")
+    private String email = null;
+
+    @Pattern(
+            regexp = "^01[016789]-\\d{3,4}-\\d{4}$",
+            message = "전화번호는 형식에 맞게 입력해주세요. 예: 010-1234-5678"
+    )
+    private String phone = null;
+
+    private Role role = null;
+
+    public UserUpdateDto toDto() {
+        return UserUpdateDto.create(this.username, this.email, this.phone, this.password, this.role);
+    }
+}
