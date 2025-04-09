6e872c84f66c4c158b796e45d635f1decaaecc91
jjsh0208 <jjsh0208@gmail.com>
2025-04-10 01:52:51 +0900

    feat : #87 Common JpaAuditingConfig 제거

--- com.taken_seat.common-service/src/main/java/com/taken_seat/common_service/config/JpaAuditingConfig.java
+++
@@ -1,9 +0,0 @@
-package com.taken_seat.common_service.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-@Configuration
-@EnableJpaAuditing
-public class JpaAuditingConfig {
-}
