38cbb224f305d2f7a22d870b7558af2d1138885d
JUNG DAYE <jungyj0809@gmail.com>
2025-04-09 18:27:02 +0900

    feat: #31 공연 전체 조회 메서드 findAll(filterParam, pageable) 정의

--- com.taken_seat.performance-service/src/main/java/com/taken_seat/performance_service/performance/domain/repository/PerformanceRepository.java
+++ com.taken_seat.performance-service/src/main/java/com/taken_seat/performance_service/performance/domain/repository/PerformanceRepository.java
@@ -3,6 +3,10 @@ package com.taken_seat.performance_service.performance.domain.repository;
 import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import com.taken_seat.performance_service.performance.application.dto.request.SearchFilterParam;
 import com.taken_seat.performance_service.performance.domain.model.Performance;
 
 public interface PerformanceRepository {
@@ -10,6 +14,8 @@ public interface PerformanceRepository {
 	Performance save(Performance performance);
 
 	Optional<Performance> findById(UUID id);
+
+	Page<Performance> findAll(SearchFilterParam filterParam, Pageable pageable);
 
 	void deleteById(UUID id, UUID deletedBy);
 }
