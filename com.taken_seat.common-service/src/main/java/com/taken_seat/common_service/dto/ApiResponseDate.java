50e94c6dce8d9912f3123b7c3e901d6e701fdc23
jjsh0208 <jjsh0208@gmail.com>
2025-04-09 16:51:05 +0900

    feat : #87 Common 공통 반환 Response 구현

---
+++ com.taken_seat.common-service/src/main/java/com/taken_seat/common_service/dto/ApiResponseDate.java
@@ -0,0 +1,16 @@
+package com.taken_seat.common_service.dto;
+
+import lombok.Getter;
+
+@Getter
+public record ApiResponseDate<T>(int status, boolean success, T body) {
+	// 성공 응답 생성
+	public static <T> ApiResponseDate<T> success(int status, T body) {
+		return new ApiResponseDate<>(status, true, body);
+	}
+
+	// 실패 응답 생성
+	public static <T> ApiResponseDate<T> failure(int status, T body) {
+		return new ApiResponseDate<>(status, false, body);
+	}
+}
