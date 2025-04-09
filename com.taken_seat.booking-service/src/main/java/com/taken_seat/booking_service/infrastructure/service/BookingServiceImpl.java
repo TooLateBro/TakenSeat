238af12ef63b5210d61686967279ec267df646ec
Goldbar97 <ehrm1357@gmail.com>
2025-04-09 12:21:33 +0900

    feat: #24 booking 단건, 리스트 조회 생성

--- com.taken_seat.booking-service/src/main/java/com/taken_seat/booking_service/infrastructure/service/BookingServiceImpl.java
+++ com.taken_seat.booking-service/src/main/java/com/taken_seat/booking_service/infrastructure/service/BookingServiceImpl.java
@@ -1,13 +1,20 @@
 package com.taken_seat.booking_service.infrastructure.service;
 
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.taken_seat.booking_service.application.BookingService;
 import com.taken_seat.booking_service.application.RedissonService;
 import com.taken_seat.booking_service.application.dto.request.BookingCreateRequest;
 import com.taken_seat.booking_service.application.dto.response.BookingCreateResponse;
+import com.taken_seat.booking_service.application.dto.response.BookingPageResponse;
+import com.taken_seat.booking_service.application.dto.response.BookingReadResponse;
 import com.taken_seat.booking_service.domain.Booking;
 import com.taken_seat.booking_service.domain.repository.BookingRepository;
 
@@ -21,6 +28,7 @@ public class BookingServiceImpl implements BookingService {
 	private final BookingRepository bookingRepository;
 
 	@Override
+	@Transactional
 	public BookingCreateResponse createBooking(BookingCreateRequest request) {
 
 		UUID userId = UUID.randomUUID();
@@ -35,5 +43,25 @@ public class BookingServiceImpl implements BookingService {
 		Booking saved = bookingRepository.save(booking);
 
 		return BookingCreateResponse.toDto(saved);
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public BookingReadResponse readBooking(UUID id) {
+
+		// TODO: 사용자의 예매만 조회할 것
+		Booking booking = bookingRepository.findById(id).orElseThrow(() -> new RuntimeException("존재하지 않는 예약입니다."));
+
+		return BookingReadResponse.toDto(booking);
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public BookingPageResponse readBookings(Pageable pageable, UUID userId) {
+
+		// TODO: 사용자의 예매만 조회할 것
+		Page<Booking> page = bookingRepository.findAllByUserId(pageable, userId);
+
+		return BookingPageResponse.toDto(page);
 	}
 }
