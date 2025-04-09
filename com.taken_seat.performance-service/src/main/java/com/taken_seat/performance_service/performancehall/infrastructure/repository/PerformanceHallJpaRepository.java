79fb00a0bc25e0057f519fb1a0fc1ea551923bd3
JUNG DAYE <jungyj0809@gmail.com>
2025-04-10 01:01:02 +0900

    feat: #35 공연장 이름과 주소로 중복 조회 가능한 JPA 메서드 추가
    - findByNameAndAddress(String name, String address) 메서드 정의

---
+++ com.taken_seat.performance-service/src/main/java/com/taken_seat/performance_service/performancehall/infrastructure/repository/PerformanceHallJpaRepository.java
@@ -0,0 +1,15 @@
+package com.taken_seat.performance_service.performancehall.infrastructure.repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import com.taken_seat.performance_service.performancehall.domain.model.PerformanceHall;
+
+public interface PerformanceHallJpaRepository
+	extends JpaRepository<PerformanceHall, UUID>, JpaSpecificationExecutor<PerformanceHall> {
+
+	Optional<PerformanceHall> findByNameAndAddress(String name, String address);
+}
