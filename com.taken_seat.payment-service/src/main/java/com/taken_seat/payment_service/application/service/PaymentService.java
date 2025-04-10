6e93461eca69f2194f758f1268cd54bbc0d64e40
jjsh0208 <jjsh0208@gmail.com>
2025-04-10 14:07:41 +0900

    feat : #132 Payment service 기능 주석 추가

--- com.taken_seat.payment-service/src/main/java/com/taken_seat/payment_service/application/service/PaymentService.java
+++ com.taken_seat.payment-service/src/main/java/com/taken_seat/payment_service/application/service/PaymentService.java
@@ -68,6 +68,17 @@ public class PaymentService {
 		return PaymentRegisterResDto.toResponse(payment);
 	}
 
+	/**
+	 * 결제 상세 조회 기능
+	 *
+	 * 주어진 결제 ID를 기반으로 상세 결제 정보를 조회한다.
+	 * 1. 삭제되지 않은 Payment를 조회하며, 존재하지않을 경우 PaymentNotFoundException을 발생시킨다.
+	 * 2. 조회된 결제를 PaymentDetailResDto로 변환하여 반환한다.
+	 *
+	 * @param id 조회할 결제 ID
+	 * @return 조회된 결제 상세 응답 DTO
+	 * @throws PaymentNotFoundException 결제 ID에 해당하는 정보가 존재하지 않는 경우 예외 발생
+	 */
 	@Transactional(readOnly = true)
 	@CachePut(cacheNames = "paymentCache", key = "#id")
 	public PaymentDetailResDto getPaymentDetail(UUID id) {
@@ -80,6 +91,23 @@ public class PaymentService {
 		return PaymentDetailResDto.toResponse(payment);
 	}
 
+	/**
+	 * 결제 검색 기능
+	 *
+	 * 주어진 조건을 기반으로 결제 목록을 검색한다. ( 캐싱 적용 )
+	 * 검색 조건에 따라 결제를 조회하고, 결과를 페이징 처리하여 반환한다.
+	 *
+	 * 이 과정에서 `@Cacheable`을 사용하여 검색 조건에 맞는 주문 목록을 캐시한다. 캐시가 존재하면 빠르게 반환하며,
+	 * 캐시가 없을 경우 DB에서 검색하여 결과를 반환한다.
+	 *
+	 * @param q 검색어
+	 * @param category 카테고리 ( 결제 상태 , 결제 성공 일자, 환불 일자 )
+	 * @param page 페이지 번호
+	 * @param size 페이지 크기
+	 * @param sort 정렬 기준
+	 * @param order 정렬 순서 (ASC/DESC)
+	 * @return 검색 결과를 포함한 PageOrderResponseDto 객체
+	 */
 	@Transactional(readOnly = true)
 	@Cacheable(cacheNames = "paymentSearchCache", key = "#q + '-' + #category + '-' + #page + '-' + #size")
 	public PagePaymentResponseDto searchPayment(String q, String category, int page, int size, String sort,
@@ -92,6 +120,24 @@ public class PaymentService {
 		return PagePaymentResponseDto.toResponse(paymentDetailResDtoPages);
 	}
 
+	/**
+	 * 결제 수정 기능
+	 *
+	 * 주어진 결제 ID와 수정 요청 DTO를 기반으로 결제 정보를 수정한다.
+	 * 1. 결제 금액이 1원 미만일 경우 IllegalArgumentException 예외를 발생시킨다.
+	 * 2. 삭제되지 않은 Payment와 관련된 PaymentHistory를 조회한다.
+	 *    - 둘 중 하나라도 없을 경우 각각 PaymentNotFoundException 또는 PaymentHistoryNotFoundException 발생
+	 * 3. 결제 정보(Payment)를 수정하고, 이력(PaymentHistory)도 함께 수정한다.
+	 * 4. 수정된 결제 정보를 PaymentUpdateResDto로 변환하여 반환한다.
+	 *
+	 * @param id 수정할 결제 ID
+	 * @param paymentUpdateReqDto 수정할 결제 정보 DTO
+	 * @param updatedBy 수정자 ID
+	 * @return 수정된 결제 응답 DTO
+	 * @throws IllegalArgumentException 결제 금액이 1원 미만인 경우 예외 발생
+	 * @throws PaymentNotFoundException 결제가 존재하지 않는 경우 예외 발생
+	 * @throws PaymentHistoryNotFoundException 결제 이력이 존재하지 않는 경우 예외 발생
+	 */
 	@CachePut(cacheNames = "paymentCache", key = "#id")
 	@CacheEvict(cacheNames = "paymentSearchCache", allEntries = true)
 	public PaymentUpdateResDto updatePayment(UUID id, PaymentUpdateReqDto paymentUpdateReqDto, UUID updatedBy) {
@@ -114,6 +160,20 @@ public class PaymentService {
 		return PaymentUpdateResDto.toResponse(payment);
 	}
 
+	/**
+	 * 결제 삭제 기능 (Soft Delete)
+	 *
+	 * 주어진 결제 ID를 기반으로 결제 및 결제 이력을 논리적으로 삭제 처리한다.
+	 * 1. 삭제되지 않은 Payment와 관련된 PaymentHistory를 조회한다.
+	 *    - 존재하지 않을 경우 각각 PaymentNotFoundException 또는 PaymentHistoryNotFoundException 발생
+	 * 2. Payment 및 PaymentHistory의 삭제 처리 메서드를 호출한다.
+	 * 3. 캐시에서 관련 결제 및 검색 캐시 제거
+	 *
+	 * @param id 삭제할 결제 ID
+	 * @param userId 삭제 처리자 ID
+	 * @throws PaymentNotFoundException 결제가 존재하지 않는 경우 예외 발생
+	 * @throws PaymentHistoryNotFoundException 결제 이력이 존재하지 않는 경우 예외 발생
+	 */
 	@Caching(evict = {
 		@CacheEvict(cacheNames = "paymentCache", key = "#id"),
 		@CacheEvict(cacheNames = "paymentSearchCache", key = "#id")
