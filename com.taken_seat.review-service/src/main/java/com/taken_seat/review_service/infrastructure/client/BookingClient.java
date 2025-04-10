52d6a604939375446f8ddf7a7522338593eefb1c
jjsh0208 <jjsh0208@gmail.com>
2025-04-10 20:13:01 +0900

    feat : #132 Review 해당 사용자가 예매한 예매의 상태를 반환하는 메서드 client 구현

---
+++ com.taken_seat.review-service/src/main/java/com/taken_seat/review_service/infrastructure/client/BookingClient.java
@@ -0,0 +1,19 @@
+package com.taken_seat.review_service.infrastructure.client;
+
+import java.util.UUID;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.taken_seat.common_service.dto.ApiResponseData;
+import com.taken_seat.review_service.infrastructure.client.dto.BookingStatusDto;
+
+@FeignClient(name = "${feign.client.booking.name}", url = "${feign.client.booking.url}")
+public interface BookingClient {
+
+	@GetMapping("/{userId}/{performanceId}/status)")
+	ResponseEntity<ApiResponseData<BookingStatusDto>> getBookingStatus(@PathVariable UUID userId,
+		@PathVariable UUID performanceId);
+}
