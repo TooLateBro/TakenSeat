d19bd0f670a7f6aded43599a2d0b6a851d033d0b
SeungGyu <seungg8361@naver.com>
2025-04-11 14:50:58 +0900

    feat : #156 Auth 100개 유저 데이터 생성
    - 디렉토리 구조 변경 및 아주 사소한 수정사항도 같이 커밋했습니다.

--- com.taken_seat.auth-service/src/main/java/com/taken_seat/auth_service/application/service/user/KafkaProducerService.java
+++ com.taken_seat.auth-service/src/main/java/com/taken_seat/auth_service/application/service/user/KafkaProducerService.java
@@ -3,12 +3,12 @@ package com.taken_seat.auth_service.application.service.user;
 import com.taken_seat.auth_service.application.dto.user.KafkaUserInfoMessage;
 import com.taken_seat.auth_service.domain.entity.user.User;
 import com.taken_seat.auth_service.domain.repository.user.UserRepository;
-import com.taken_seat.auth_service.presentation.dto.user.KafkaUserCouponRequestMessage;
+import com.taken_seat.common_service.message.KafkaUserCouponMessage;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
-public class KafkaUserService {
+public class KafkaProducerService {
 
     private final KafkaTemplate<String, Object> kafkaTemplate;
     private final UserRepository userRepository;
@@ -17,12 +17,12 @@ public class KafkaUserService {
     private static final String REQUEST_KEY = "Partitions-of-coupons";
     private static final String RESPONSE_TOPIC = "coupon-to-user";
 
-    public KafkaUserService(KafkaTemplate<String, Object> kafkaTemplate, UserRepository userRepository) {
+    public KafkaProducerService(KafkaTemplate<String, Object> kafkaTemplate, UserRepository userRepository) {
         this.kafkaTemplate = kafkaTemplate;
         this.userRepository = userRepository;
     }
 
-    public KafkaUserInfoMessage sendUserCoupon(KafkaUserCouponRequestMessage message) {
+    public KafkaUserInfoMessage sendUserCoupon(KafkaUserCouponMessage message) {
         User user = userRepository.findByIdAndDeletedAtIsNull(message.getUserId())
                 .orElseThrow(() -> new RuntimeException("사용자를 찾을 수 없습니다."));
         kafkaTemplate.send(REQUEST_TOPIC, REQUEST_KEY, message);
--- com.taken_seat.auth-service/src/main/java/com/taken_seat/auth_service/presentation/controller/user/KafkaProducerController.java
+++ com.taken_seat.auth-service/src/main/java/com/taken_seat/auth_service/presentation/controller/user/KafkaProducerController.java
@@ -1,9 +1,9 @@
 package com.taken_seat.auth_service.presentation.controller.user;
 
 import com.taken_seat.auth_service.application.dto.user.KafkaUserInfoMessage;
-import com.taken_seat.auth_service.application.service.user.KafkaUserService;
-import com.taken_seat.auth_service.presentation.dto.user.KafkaUserCouponRequestMessage;
+import com.taken_seat.auth_service.application.service.user.KafkaProducerService;
 import com.taken_seat.common_service.dto.ApiResponseData;
+import com.taken_seat.common_service.message.KafkaUserCouponMessage;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,17 +12,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/users")
-public class KafkaUserController {
+public class KafkaProducerController {
 
-    private final KafkaUserService kafkaUserService;
+    private final KafkaProducerService kafkaProducerService;
 
-    public KafkaUserController(KafkaUserService kafkaUserService) {
-        this.kafkaUserService = kafkaUserService;
+    public KafkaProducerController(KafkaProducerService kafkaProducerService) {
+        this.kafkaProducerService = kafkaProducerService;
     }
 
     @GetMapping("/send")
-    public ResponseEntity<ApiResponseData<KafkaUserInfoMessage>> sendUserCoupon(@RequestBody KafkaUserCouponRequestMessage message){
-        KafkaUserInfoMessage coupon = kafkaUserService.sendUserCoupon(message);
+    public ResponseEntity<ApiResponseData<KafkaUserInfoMessage>> sendUserCoupon(
+            @RequestBody KafkaUserCouponMessage message){
+
+        KafkaUserInfoMessage coupon = kafkaProducerService.sendUserCoupon(message);
 
         return ResponseEntity.ok().body(ApiResponseData.success(coupon));
     }
