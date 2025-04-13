06f18fce9b77370351f53ccfa4174d1791210785
JUNG DAYE <jungyj0809@gmail.com>
2025-04-13 23:04:39 +0900

    feat: #167 좌석 선점 및 선점 취소 API 컨트롤러 추가
    - PUT /api/v1/performancehalls/seat/status : 좌석 선점 요청 처리
    - PUT /api/v1/performancehalls/seat/status/cancel : 좌석 선점 취소 요청 처리
    - FeignClient를 통해 외부 예매 서비스와 연동 가능하도록 구성

--- com.taken_seat.performance-service/src/main/java/com/taken_seat/performance_service/performancehall/presentation/controller/PerformanceHallController.java
+++ com.taken_seat.performance-service/src/main/java/com/taken_seat/performance_service/performancehall/presentation/controller/PerformanceHallController.java
@@ -13,12 +13,15 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.taken_seat.common_service.dto.ApiResponseData;
 import com.taken_seat.common_service.dto.AuthenticatedUser;
+import com.taken_seat.common_service.dto.request.BookingSeatClientRequestDto;
+import com.taken_seat.common_service.dto.response.BookingSeatClientResponseDto;
 import com.taken_seat.performance_service.performancehall.application.dto.request.CreateRequestDto;
 import com.taken_seat.performance_service.performancehall.application.dto.request.SearchFilterParam;
 import com.taken_seat.performance_service.performancehall.application.dto.request.UpdateRequestDto;
@@ -79,4 +82,21 @@ public class PerformanceHallController {
 		performanceHallService.delete(id, authenticatedUser);
 		return ResponseEntity.status(HttpStatus.NO_CONTENT).body(ApiResponseData.success());
 	}
+
+	@PutMapping("/seat/status")
+	public ResponseEntity<ApiResponseData<BookingSeatClientResponseDto>> updateSeatStatus(
+		@Valid @RequestBody BookingSeatClientRequestDto request) {
+
+		BookingSeatClientResponseDto response = performanceHallService.updateSeatStatus(request);
+		return ResponseEntity.status(HttpStatus.OK).body(ApiResponseData.success(response));
+	}
+
+	@PutMapping("/seat/status/cancel")
+	public ResponseEntity<ApiResponseData<BookingSeatClientResponseDto>> cancelSeatStatus(
+		@Valid @RequestBody BookingSeatClientRequestDto request) {
+
+		BookingSeatClientResponseDto response = performanceHallService.cancelSeatStatus(request);
+		return ResponseEntity.status(HttpStatus.OK).body(ApiResponseData.success(response));
+	}
+
 }
