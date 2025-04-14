8664b5bffcdabbb26abcd7da70f9a60ab92ae442
Goldbar97 <ehrm1357@gmail.com>
2025-04-14 18:51:09 +0900

    feat: #161 <Booking>  responsecode 추가

--- com.taken_seat.common-service/src/main/java/com/taken_seat/common_service/message/PaymentRequestMessage.java
+++ com.taken_seat.common-service/src/main/java/com/taken_seat/common_service/message/PaymentRequestMessage.java
@@ -3,10 +3,12 @@ package com.taken_seat.common_service.message;
 import java.util.UUID;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class PaymentRequestMessage {
@@ -20,5 +22,4 @@ public class PaymentRequestMessage {
 	private Integer mileage;
 
 	private Integer price; // 좌석 금액( 원본 )
-
 }
--- com.taken_seat.common-service/src/main/java/com/taken_seat/common_service/message/PaymentResultMessage.java
+++ com.taken_seat.common-service/src/main/java/com/taken_seat/common_service/message/PaymentResultMessage.java
@@ -18,5 +18,4 @@ public class PaymentResultMessage {
 	private UUID paymentId;
 
 	private PaymentResultStatus status;
-
 }
