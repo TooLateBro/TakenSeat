9d556b761b79ee8daea1707c57da1e7fef1576bd
dkki4887 <4887chaeg@gmail.com>
2025-04-15 23:55:42 +0900

    feat: #222 대기열 DTO 생성 - QueueReqDto, RankDto 구현

---
+++ com.taken_seat.queue-service/src/main/java/com/taken_seat/queue_service/application/dto/QueueReqDto.java
@@ -0,0 +1,17 @@
+package com.taken_seat.queue_service.application.dto;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.UUID;
+
+@AllArgsConstructor
+@Getter
+public class QueueReqDto {
+    @NotNull(message = "공연 회차 ID는 필수값입니다.")
+    private UUID performanceId = UUID.randomUUID();
+
+    @NotNull(message = "공연 회차 ID는 필수값입니다.")
+    private UUID performanceScheduleId = UUID.randomUUID();
+}
