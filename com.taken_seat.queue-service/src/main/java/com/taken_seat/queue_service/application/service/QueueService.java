e14190491925e5fd26ca785789a14423889fa696
dkki4887 <4887chaeg@gmail.com>
2025-04-16 00:41:45 +0900

    feat: #226 import 추가 - 실수로 import 삭제해버려서 다시 추가했습니다..

--- com.taken_seat.queue-service/src/main/java/com/taken_seat/queue_service/application/service/QueueService.java
+++ com.taken_seat.queue-service/src/main/java/com/taken_seat/queue_service/application/service/QueueService.java
@@ -7,6 +7,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 
 @Slf4j
