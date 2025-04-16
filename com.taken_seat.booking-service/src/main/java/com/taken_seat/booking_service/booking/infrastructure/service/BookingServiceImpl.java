fad5c2d4f53deb7eb5cdbdb2f7fc10613c9669e5
JUNG DAYE <jungyj0809@gmail.com>
2025-04-16 18:48:24 +0900

    feat: #217 좌석 선점 및 예매 흐름에 로그 추가
    - PerformanceHallService: 좌석 배치도 조회, 선점/취소 시 로그 추가
    - RedissonServiceImpl: 좌석 락 획득, 해제, 실패 로그 추가
    - BookingServiceImpl: 예매 생성 및 주요 상태 변경 시 로그 기록 보강

--- com.taken_seat.booking-service/src/main/java/com/taken_seat/booking_service/booking/infrastructure/service/BookingServiceImpl.java
+++ com.taken_seat.booking-service/src/main/java/com/taken_seat/booking_service/booking/infrastructure/service/BookingServiceImpl.java
@@ -31,7 +31,9 @@ import com.taken_seat.common_service.message.PaymentMessage;
 import com.taken_seat.common_service.message.UserBenefitMessage;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class BookingServiceImpl implements BookingService {
@@ -44,6 +46,10 @@ public class BookingServiceImpl implements BookingService {
 	@Override
 	@Transactional
 	public BookingCreateResponse createBooking(AuthenticatedUser authenticatedUser, BookingCreateRequest request) {
+
+		log.info("[Booking] 예매 요청 - 시작 - userId={}, performanceId={}, scheduleId={}, seatId={}",
+			authenticatedUser.getUserId(), request.getPerformanceId(),
+			request.getPerformanceScheduleId(), request.getSeatId());
 
 		BookingSeatClientResponseDto responseDto = redissonService.tryHoldSeat(
 			request.getPerformanceId(),
@@ -61,6 +67,12 @@ public class BookingServiceImpl implements BookingService {
 			.build();
 
 		Booking saved = bookingRepository.save(booking);
+
+		log.info(
+			"[Booking] 예매 생성 - 성공 - bookingId={}, userId={}, performanceScheduleId={}, seatId={}, price={}",
+			saved.getId(), saved.getUserId(), saved.getPerformanceScheduleId(), saved.getSeatId(), saved.getPrice());
+
+		System.out.println("dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd" + saved.getId());
 
 		return BookingCreateResponse.toDto(saved);
 	}
