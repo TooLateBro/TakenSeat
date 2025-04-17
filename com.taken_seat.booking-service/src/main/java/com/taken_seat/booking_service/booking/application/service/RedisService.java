c875f9cc0f8f2b200c305d0470c52f96fbaeb2ce
Goldbar97 <ehrm1357@gmail.com>
2025-04-17 18:20:19 +0900

    refactor: #161 <Booking> yml 에서 변수를 가져오도록 수정

--- com.taken_seat.booking-service/src/main/java/com/taken_seat/booking_service/booking/application/service/RedisService.java
+++ com.taken_seat.booking-service/src/main/java/com/taken_seat/booking_service/booking/application/service/RedisService.java
@@ -3,6 +3,7 @@ package com.taken_seat.booking_service.booking.application.service;
 import java.time.Duration;
 import java.util.UUID;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
@@ -14,7 +15,10 @@ public class RedisService {
 
 	private final RedisTemplate<String, String> redisTemplate;
 
+	@Value("${variable.booking-expiration-time}")
+	private long BOOKING_EXPIRATION_TIME;
+
 	public void setBookingExpire(UUID bookingId) {
-		redisTemplate.opsForValue().set("expire:" + bookingId, "", Duration.ofMinutes(10));
+		redisTemplate.opsForValue().set("expire:" + bookingId, "", Duration.ofSeconds(BOOKING_EXPIRATION_TIME));
 	}
 }
