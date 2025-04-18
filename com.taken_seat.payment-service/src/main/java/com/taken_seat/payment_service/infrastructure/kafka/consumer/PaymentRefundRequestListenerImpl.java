a20e5b0521df155413e22e582a47abacf5a25b84
jjsh0208 <jjsh0208@gmail.com>
2025-04-19 00:22:41 +0900

    fix : #304 Payment 결제 환불 이벤트 리스너에 누락된 서비스 추가

--- com.taken_seat.payment-service/src/main/java/com/taken_seat/payment_service/infrastructure/kafka/consumer/PaymentRefundRequestListenerImpl.java
+++ com.taken_seat.payment-service/src/main/java/com/taken_seat/payment_service/infrastructure/kafka/consumer/PaymentRefundRequestListenerImpl.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Component;
 
 import com.taken_seat.common_service.message.PaymentRefundMessage;
 import com.taken_seat.payment_service.application.kafka.consumer.PaymentRefundRequestListener;
+import com.taken_seat.payment_service.application.service.PaymentRefundEventHandlerService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -12,9 +13,11 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class PaymentRefundRequestListenerImpl implements PaymentRefundRequestListener {
 
+	private final PaymentRefundEventHandlerService paymentRefundEventHandlerService;
+
 	@Override
 	@KafkaListener(topics = "${kafka.topic.refund-request}", groupId = "${kafka.consumer.group-id}")
 	public void handlerRefundRequest(PaymentRefundMessage message) {
-
+		paymentRefundEventHandlerService.processPaymentRefund(message);
 	}
 }
