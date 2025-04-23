09001d80f5642cd2d3b16c7149280028c8be23c8
SeungGyu <seungg8361@naver.com>
2025-04-23 19:29:06 +0900

    refactor : #360 <Coupon> 쿠폰 DTO를 record로 변경 및 validation 추가
    - DTO를 record로 변경
    - DTO에 Optional제거 후 validation 추가

--- com.taken_seat.coupon-service/src/main/java/com/taken_seat/coupon_service/presentation/dto/CreateCouponRequestDto.java
+++ com.taken_seat.coupon-service/src/main/java/com/taken_seat/coupon_service/presentation/dto/CreateCouponRequestDto.java
@@ -2,31 +2,27 @@ package com.taken_seat.coupon_service.presentation.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.taken_seat.coupon_service.application.dto.CouponDto;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Size;
 
 import java.time.LocalDateTime;
 
-@Getter
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
-public class CreateCouponRequestDto {
+public record CreateCouponRequestDto(
+        @Size(min = 1, max = 100, message = "쿠폰 이름은 1자 이상 100자 이하로 입력해주세요.")
+         String name,
 
-    
-    private String name;
+        @Size(min = 1, max = 50, message = "코드는 1자 이상 50자 이하로 입력해주세요.")
+        String code,
 
-    private String code;
+        @Min(value = 1, message = "수량은 1 이상이어야 합니다.")
+        Long quantity,
 
-    private Long quantity;
+        @Min(value = 1, message = "할인율은 1 이상이어야 합니다.")
+        Integer discount,
 
-    private Integer discount;
-
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss.SSSSSS")
-    private LocalDateTime expiredAt;
-
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+        LocalDateTime expiredAt
+) {
     public CouponDto toDto(){
         return CouponDto.create(this.name, this.code, this.quantity, this.discount, this.expiredAt);
     }
