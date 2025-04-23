8a3c0fff10ab23d2c66d1f360259c4c226fd740b
SeungGyu <seungg8361@naver.com>
2025-04-23 12:24:17 +0900

    refactor : #352 <Auth> 회원가입 DTO record로 리팩토링
    - record를 사용하면서 기존 lombok 어노테이션을 지웠습니다.
    - builder를 사용하는 것보다 기본 생성자를 사용할 수 있도록 수정했습니다.

--- com.taken_seat.auth-service/src/main/java/com/taken_seat/auth_service/application/dto/auth/AuthSignUpDto.java
+++ com.taken_seat.auth-service/src/main/java/com/taken_seat/auth_service/application/dto/auth/AuthSignUpDto.java
@@ -1,29 +1,15 @@
 package com.taken_seat.auth_service.application.dto.auth;
 
 import com.taken_seat.auth_service.domain.vo.Role;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
 
-@AllArgsConstructor
-@Getter
-@Builder(access = AccessLevel.PRIVATE)
-public class AuthSignUpDto {
-
-    private String username;
-    private String email;
-    private String phone;
-    private String password;
-    private Role role;
-
+public record AuthSignUpDto (
+         String username,
+         String email,
+         String phone,
+         String password,
+         Role role
+){
     public static AuthSignUpDto create(String username, String email, String phone, String password, Role role) {
-        return AuthSignUpDto.builder()
-                .username(username)
-                .email(email)
-                .phone(phone)
-                .password(password)
-                .role(role)
-                .build();
+        return new AuthSignUpDto(username, email, phone, password, role);
     }
 }
