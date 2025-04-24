eb4cc4d8dea086bc64cc2441adc343e8a3cadba8
SeungGyu <seungg8361@naver.com>
2025-04-24 15:30:00 +0900

    test : #373 <Mileage> 마일리지 단건 조회 테스트
    - Role 검증 로직 리팩토링 했습니다.

--- com.taken_seat.auth-service/src/test/java/com/taken_seat/auth_service/unit/MileageServiceTest.java
+++ com.taken_seat.auth-service/src/test/java/com/taken_seat/auth_service/unit/MileageServiceTest.java
@@ -2,6 +2,7 @@ package com.taken_seat.auth_service.unit;
 
 import com.taken_seat.auth_service.application.dto.mileage.UserMileageResponseDto;
 import com.taken_seat.auth_service.application.service.mileage.MileageServiceImpl;
+import com.taken_seat.auth_service.domain.entity.mileage.Mileage;
 import com.taken_seat.auth_service.domain.entity.user.User;
 import com.taken_seat.auth_service.domain.repository.mileage.MileageQueryRepository;
 import com.taken_seat.auth_service.domain.repository.mileage.MileageRepository;
@@ -10,6 +11,7 @@ import com.taken_seat.auth_service.domain.vo.Role;
 import com.taken_seat.auth_service.presentation.dto.mileage.UserMileageRequestDto;
 import com.taken_seat.common_service.exception.customException.AuthException;
 import com.taken_seat.common_service.exception.enums.ResponseCode;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -22,6 +24,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 
@@ -41,17 +44,24 @@ public class MileageServiceTest {
     private MileageServiceImpl mileageService;
 
     private User user;
+    private Mileage mileage;
+
     private UUID userId;
     private UUID mileageId;
+
+    @BeforeEach
+    public void setUp() {
+        user = User.create(
+                "testuser1","test@test.com","010-1111-1111"
+                ,"testPassword1!", Role.ADMIN
+        );
+    }
 
     @Test
     @DisplayName("마일리지 생성 성공 테스트")
     public void createMileageSuccess() {
         userId = UUID.randomUUID();
-        user = User.create(
-                "testuser1","test@test.com","010-1111-1111"
-                ,"testPassword1!", Role.ADMIN
-        );
+
         UserMileageRequestDto mileageRequestDto = new UserMileageRequestDto(
                 30000
         );
@@ -73,5 +83,33 @@ public class MileageServiceTest {
         AuthException exception = assertThrows(AuthException.class, () -> mileageService.createMileageUser(userId, mileageRequestDto.toDto()));
 
         assertEquals(ResponseCode.USER_NOT_FOUND, exception.getErrorCode());
+    }
+
+    @Test
+    @DisplayName("마일리지 단건 조회 성공 테스트")
+    public void getMileageUserSuccess() {
+        mileageId = UUID.randomUUID();
+
+        Mileage mockMileage = Mockito.mock(Mileage.class);
+        when(mockMileage.getId()).thenReturn(mileageId);
+        when(mockMileage.getUser()).thenReturn(user);
+        when(mockMileage.getMileage()).thenReturn(30000);
+
+        when(mileageRepository.findByIdAndDeletedAtIsNull(mileageId)).thenReturn(Optional.of(mockMileage));
+
+        UserMileageResponseDto responseDto = mileageService.getMileageUser(mileageId);
+
+        assertNotNull(responseDto);
+    }
+    @Test
+    @DisplayName("마일리지 단건 조회 실패 테스트 - 마일리지 없음")
+    public void getMileageUserFail_MileageNotFound() {
+        mileageId = UUID.randomUUID();
+
+        when(mileageRepository.findByIdAndDeletedAtIsNull(mileageId)).thenReturn(Optional.empty());
+
+        AuthException exception = assertThrows(AuthException.class, () -> mileageService.getMileageUser(mileageId));
+
+        assertEquals(ResponseCode.MILEAGE_NOT_FOUND, exception.getErrorCode());
     }
 }
