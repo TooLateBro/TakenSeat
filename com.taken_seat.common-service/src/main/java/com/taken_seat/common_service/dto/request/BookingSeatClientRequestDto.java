8a565ad51a81e73c0e00b29b4ae14afb9bc5a788
JUNG DAYE <jungyj0809@gmail.com>
2025-04-24 17:21:06 +0900

    fix: #367 BookingSeatClientRequestDto 역직렬화 오류 해결
    - @NoArgsConstructor, @AllArgsConstructor
      기본 생성자 추가하여 Jackson에서 DTO 변환 가능하도록 수정

--- com.taken_seat.common-service/src/main/java/com/taken_seat/common_service/dto/request/BookingSeatClientRequestDto.java
+++ com.taken_seat.common-service/src/main/java/com/taken_seat/common_service/dto/request/BookingSeatClientRequestDto.java
@@ -2,11 +2,15 @@ package com.taken_seat.common_service.dto.request;
 
 import java.util.UUID;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class BookingSeatClientRequestDto {
 
 	private UUID performanceId;
