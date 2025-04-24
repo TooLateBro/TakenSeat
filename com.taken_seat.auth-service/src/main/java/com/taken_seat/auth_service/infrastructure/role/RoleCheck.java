627d428cb8e917844816c4cb1085fed26a2ebe2b
SeungGyu <seungg8361@naver.com>
2025-04-25 01:09:36 +0900

    refactor : <Mileage> 권한 체킹에 AOP 활용해서 코드 리팩토링
    - 중복 로직을 제거했습니다.
    - 관심도를 분리하여 유지보수성과 확장성을 챙겼습니다.
    - 커스텀 어노테이션을 활용해서 권한을 체킹합니다.

---
+++ com.taken_seat.auth-service/src/main/java/com/taken_seat/auth_service/infrastructure/role/RoleCheck.java
@@ -0,0 +1,18 @@
+package com.taken_seat.auth_service.infrastructure.role;
+
+import com.taken_seat.auth_service.domain.vo.Role;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+// 어노테이션이 메서드와 클래스에 모두 적용될 수 있도록 지정
+@Target({ElementType.METHOD, ElementType.TYPE})
+// 어노테이션 정보를 런타임까지 유지 (AOP 에서 사용 가능)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface RoleCheck {
+
+    // 허용된 역할 목록 설정 (기본은 빈 배열 = 모든 역할 허용)
+    Role[] allowedRoles() default {};
+}
