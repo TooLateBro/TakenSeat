21be7786fff467816636930aaa94400e12bd159a
JUNG DAYE <jungyj0809@gmail.com>
2025-04-25 14:17:03 +0900

    feat: #386 [PerformanceSchedule에 ScheduleSeat 연동] 회차 좌석 응답 DTO 생성
    - 좌석 번호, 행, 타입, 상태, 가격을 포함하는 ScheduleSeatResponseDto 생성
    - 클라이언트에게 회차 좌석 상세 정보 제공을 위한 구조 설계

--- com.taken_seat.performance-service/src/main/java/com/taken_seat/performance_service/performance/presentation/dto/response/ScheduleSeatResponseDto.java
+++ com.taken_seat.performance-service/src/main/java/com/taken_seat/performance_service/performance/presentation/dto/response/ScheduleSeatResponseDto.java
@@ -2,11 +2,16 @@ package com.taken_seat.performance_service.performance.presentation.dto.response
 
 import java.util.UUID;
 
+import com.taken_seat.performance_service.performancehall.domain.model.SeatStatus;
 import com.taken_seat.performance_service.performancehall.domain.model.SeatType;
 
-public record SeatPriceResponseDto(
-	UUID performanceSeatPriceId,
+public record ScheduleSeatResponseDto(
+	UUID id,
+	String rowNumber,
+	String seatNumber,
 	SeatType seatType,
+	SeatStatus seatStatus,
 	Integer price
 ) {
 }
+
