6ef5768ac5726fea758f05479ccea6b121675768
JUNG DAYE <jungyj0809@gmail.com>
2025-04-25 14:42:35 +0900

    delete: #386 [ScheduleSeat 기반 구조 전환] 구조 전환하면서 이전 파일 삭제

--- com.taken_seat.performance-service/src/main/java/com/taken_seat/performance_service/performance/application/dto/command/CreateSeatPriceCommand.java
+++
@@ -1,9 +0,0 @@
-package com.taken_seat.performance_service.performance.application.dto.command;
-
-import com.taken_seat.performance_service.performancehall.domain.model.SeatType;
-
-public record CreateSeatPriceCommand(
-	SeatType seatType,
-	Integer price
-) {
-}
