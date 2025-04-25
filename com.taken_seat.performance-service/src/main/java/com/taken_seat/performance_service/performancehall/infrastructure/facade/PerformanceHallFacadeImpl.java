63624aaab60571a5281d73a036f7ef61c86a99d8
JUNG DAYE <jungyj0809@gmail.com>
2025-04-25 12:17:00 +0900

    feat: #386 [ScheduleSeat 생성] 좌석 템플릿 조회 기능 추가
    - validateByPerformanceHallId를 통해 PerformanceHall 조회
    - toSeatTemplateInfos 호출하여 반환

--- com.taken_seat.performance-service/src/main/java/com/taken_seat/performance_service/performancehall/infrastructure/facade/PerformanceHallFacadeImpl.java
+++ com.taken_seat.performance-service/src/main/java/com/taken_seat/performance_service/performancehall/infrastructure/facade/PerformanceHallFacadeImpl.java
@@ -1,9 +1,11 @@
 package com.taken_seat.performance_service.performancehall.infrastructure.facade;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.stereotype.Component;
 
+import com.taken_seat.performance_service.performancehall.application.dto.command.SeatTemplateInfo;
 import com.taken_seat.performance_service.performancehall.domain.facade.PerformanceHallFacade;
 import com.taken_seat.performance_service.performancehall.domain.model.PerformanceHall;
 import com.taken_seat.performance_service.performancehall.domain.validation.PerformanceHallExistenceValidator;
@@ -26,5 +28,13 @@ public class PerformanceHallFacadeImpl implements PerformanceHallFacade {
 		PerformanceHall performanceHall = performanceHallExistenceValidator.validateByPerformanceHallId(
 			performanceHallId);
 		return performanceHall.isSoldOut();
+	}
+
+	@Override
+	public List<SeatTemplateInfo> getSeatTemplate(UUID performanceHallId) {
+		PerformanceHall performanceHall = performanceHallExistenceValidator.validateByPerformanceHallId(
+			performanceHallId
+		);
+		return performanceHall.toSeatTemplateInfos();
 	}
 }
