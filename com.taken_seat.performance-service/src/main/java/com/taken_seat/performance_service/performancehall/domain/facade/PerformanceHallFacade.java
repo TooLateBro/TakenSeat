f43eab051a2b54986f525e3e0158f0b242bd3e4c
JUNG DAYE <jungyj0809@gmail.com>
2025-04-25 12:16:47 +0900

    feat: #386 [ScheduleSeat 생성] 좌석 템플릿 조회용 메서드 정의
    - 공연장 ID 기반으로 SeatTemplateInfo 리스트 반환하는 메서드 정의

--- com.taken_seat.performance-service/src/main/java/com/taken_seat/performance_service/performancehall/domain/facade/PerformanceHallFacade.java
+++ com.taken_seat.performance-service/src/main/java/com/taken_seat/performance_service/performancehall/domain/facade/PerformanceHallFacade.java
@@ -1,7 +1,9 @@
 package com.taken_seat.performance_service.performancehall.domain.facade;
 
+import java.util.List;
 import java.util.UUID;
 
+import com.taken_seat.performance_service.performancehall.application.dto.command.SeatTemplateInfo;
 import com.taken_seat.performance_service.performancehall.domain.model.PerformanceHall;
 
 public interface PerformanceHallFacade {
@@ -9,4 +11,6 @@ public interface PerformanceHallFacade {
 	PerformanceHall getByPerformanceHallId(UUID performanceHallId);
 
 	boolean isSoldOut(UUID performanceHallId);
+
+	List<SeatTemplateInfo> getSeatTemplate(UUID performanceHallId);
 }
