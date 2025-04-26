4c0e0838532cc3d95c01d9c74a61396efff9025e
JUNG DAYE <jungyj0809@gmail.com>
2025-04-26 21:12:56 +0900

    refactor: #386 [티켓 응답 구조 변경] TicketPerformanceClientResponse record로 수정
    - TicketPerformanceClientResponse를 record 타입으로 변경
    - 공연명, 공연장명, 주소, 좌석 정보(row/seat/seatType), 시작·종료시간 포함
    - LocalDateTime 포맷 적용

--- com.taken_seat.common-service/src/main/java/com/taken_seat/common_service/dto/response/TicketPerformanceClientResponse.java
+++ com.taken_seat.common-service/src/main/java/com/taken_seat/common_service/dto/response/TicketPerformanceClientResponse.java
@@ -4,30 +4,21 @@ import java.time.LocalDateTime;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-@AllArgsConstructor
-@Builder
-@Getter
-@NoArgsConstructor
-public class TicketPerformanceClientResponse {
-
-	private String title;
-	private String name;
-	private String address;
-	private String seatRowNumber;
-	private String seatNumber;
-	private String seatType;
+public record TicketPerformanceClientResponse(
+	String title,
+	String name,
+	String address,
+	String rowNumber,
+	String seatNumber,
+	String seatType,
 
 	/**
 	 * 공연 DB와 도메인에 LocalDateTime 을 사용하고 있어서 동일하게 적용
 	 */
 	@JsonFormat(pattern = "yyyy-MM-dd HH:mm")
-	private LocalDateTime startAt;
+	LocalDateTime startAt,
 
 	@JsonFormat(pattern = "yyyy-MM-dd HH:mm")
-	private LocalDateTime endAt;
+	LocalDateTime endAt
+) {
 }
