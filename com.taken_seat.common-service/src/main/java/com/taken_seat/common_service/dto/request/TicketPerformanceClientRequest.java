d1246da015eb3b0e8f394897dfcaa8ae128c8fc9
JUNG DAYE <jungyj0809@gmail.com>
2025-04-26 21:11:14 +0900

    refactor: #386 [좌석 ID 변경] TicketPerformanceClientRequest scheduleSeatId로 수정
    - TicketPerformanceClientRequest를 record 타입으로 변경
    - seatId → scheduleSeatId로 필드명 수정
    - 예매 요청 시 공연 회차별 좌석 ID 사용

--- com.taken_seat.common-service/src/main/java/com/taken_seat/common_service/dto/request/TicketPerformanceClientRequest.java
+++ com.taken_seat.common-service/src/main/java/com/taken_seat/common_service/dto/request/TicketPerformanceClientRequest.java
@@ -2,14 +2,9 @@ package com.taken_seat.common_service.dto.request;
 
 import java.util.UUID;
 
-import lombok.Builder;
-import lombok.Getter;
-
-@Getter
-@Builder
-public class TicketPerformanceClientRequest {
-
-	private final UUID performanceId;
-	private final UUID performanceScheduleId;
-	private final UUID seatId;
+public record TicketPerformanceClientRequest(
+	UUID performanceId,
+	UUID performanceScheduleId,
+	UUID scheduleSeatId
+) {
 }
