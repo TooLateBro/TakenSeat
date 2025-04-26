bebfa32c9fd0efb00ec653c85e08fc599795cca2
jjsh0208 <jjsh0208@gmail.com>
2025-04-26 18:39:36 +0900

    refactor : #395 Payment MapStruct dto 변환 과정 추가

--- com.taken_seat.payment-service/src/main/java/com/taken_seat/payment_service/presentation/controller/PaymentController.java
+++ com.taken_seat.payment-service/src/main/java/com/taken_seat/payment_service/presentation/controller/PaymentController.java
@@ -6,12 +6,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.taken_seat.common_service.aop.annotation.RoleCheck;
@@ -19,10 +19,13 @@ import com.taken_seat.common_service.aop.vo.Role;
 import com.taken_seat.common_service.dto.ApiResponseData;
 import com.taken_seat.common_service.dto.AuthenticatedUser;
 import com.taken_seat.payment_service.application.dto.request.PaymentRegisterReqDto;
+import com.taken_seat.payment_service.application.dto.request.PaymentSearchReqDto;
 import com.taken_seat.payment_service.application.dto.request.PaymentUpdateReqDto;
 import com.taken_seat.payment_service.application.dto.response.PagePaymentResponseDto;
 import com.taken_seat.payment_service.application.dto.response.PaymentDetailResDto;
+import com.taken_seat.payment_service.application.dto.service.PaymentDto;
 import com.taken_seat.payment_service.application.service.PaymentService;
+import com.taken_seat.payment_service.infrastructure.mapper.PaymentMapper;
 import com.taken_seat.payment_service.infrastructure.swagger.PaymentSwaggerDocs;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -36,6 +39,7 @@ import lombok.RequiredArgsConstructor;
 public class PaymentController {
 
 	private final PaymentService paymentService;
+	private final PaymentMapper paymentMapper;
 
 	@PostMapping
 	@RoleCheck(allowedRoles = Role.ADMIN)
@@ -44,52 +48,52 @@ public class PaymentController {
 		@Valid @RequestBody PaymentRegisterReqDto paymentRegisterReqDto,
 		AuthenticatedUser authenticatedUser) {
 
+		PaymentDto dto = paymentMapper.toDto(paymentRegisterReqDto, authenticatedUser);
+
 		return ResponseEntity.status(HttpStatus.OK)
 			.body(
-				ApiResponseData.success(paymentService.registerPayment(paymentRegisterReqDto, authenticatedUser)));
+				ApiResponseData.success(paymentService.registerPayment(dto)));
 	}
 
-	@GetMapping("/{id}")
+	@GetMapping("/{paymentId}")
 	@PaymentSwaggerDocs.GetPaymentDetail
-	public ResponseEntity<ApiResponseData<PaymentDetailResDto>> getPaymentDetail(@PathVariable("id") UUID id) {
+	public ResponseEntity<ApiResponseData<PaymentDetailResDto>> getPaymentDetail(
+		@PathVariable("paymentId") UUID paymentId) {
 		return ResponseEntity.status(HttpStatus.OK)
 			.body(
-				ApiResponseData.success(paymentService.getPaymentDetail(id)));
+				ApiResponseData.success(paymentService.getPaymentDetail(paymentId)));
 	}
 
 	@GetMapping("/search")
 	@PaymentSwaggerDocs.SearchPayment
 	public ResponseEntity<ApiResponseData<PagePaymentResponseDto>> searchPayment(
-		@RequestParam(required = false) String q,
-		@RequestParam(required = false) String category,
-		@RequestParam(defaultValue = "0") int page,
-		@RequestParam(defaultValue = "10") int size,
-		@RequestParam(defaultValue = "createdAt") String sort,
-		@RequestParam(defaultValue = "desc") String order) {
+		@ModelAttribute PaymentSearchReqDto searchReqDto) {
 		return ResponseEntity.status(HttpStatus.OK)
 			.body(
-				ApiResponseData.success(paymentService.searchPayment(q, category, page, size, sort, order)));
+				ApiResponseData.success(paymentService.searchPayment(searchReqDto)));
 	}
 
-	@PatchMapping("/{id}")
+	@PatchMapping("/{paymentId}")
 	@RoleCheck(allowedRoles = Role.ADMIN)
 	@PaymentSwaggerDocs.UpdatePayment
-	public ResponseEntity<ApiResponseData<PaymentDetailResDto>> updatePayment(@PathVariable("id") UUID id,
+	public ResponseEntity<ApiResponseData<PaymentDetailResDto>> updatePayment(@PathVariable("paymentId") UUID paymentId,
 		@Valid @RequestBody PaymentUpdateReqDto paymentUpdateReqDto,
 		AuthenticatedUser authenticatedUser) {
 
+		PaymentDto dto = paymentMapper.toDto(paymentId, paymentUpdateReqDto, authenticatedUser);
+
 		return ResponseEntity.status(HttpStatus.OK)
 			.body(
-				ApiResponseData.success(paymentService.updatePayment(id, paymentUpdateReqDto, authenticatedUser)));
+				ApiResponseData.success(paymentService.updatePayment(dto)));
 
 	}
 
-	@DeleteMapping("/{id}")
+	@DeleteMapping("/{paymentId}")
 	@RoleCheck(allowedRoles = Role.ADMIN)
 	@PaymentSwaggerDocs.DeletePayment
-	public ResponseEntity<ApiResponseData<String>> deletePayment(@PathVariable("id") UUID id,
+	public ResponseEntity<ApiResponseData<String>> deletePayment(@PathVariable("paymentId") UUID paymentId,
 		AuthenticatedUser authenticatedUser) {
-		paymentService.deletePayment(id, authenticatedUser);
+		paymentService.deletePayment(paymentId, authenticatedUser);
 		return ResponseEntity.status(HttpStatus.OK)
 			.body(
 				ApiResponseData.success("Delete Success"));
