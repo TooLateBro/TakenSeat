d56228c199152d4ec313e8767c5b9ea29078c364
Goldbar97 <ehrm1357@gmail.com>
2025-04-28 01:47:02 +0900

    feat: #161 <Booking> command 서비스로 구분

---
+++ com.taken_seat.booking-service/src/main/java/com/taken_seat/booking_service/booking/infrastructure/service/BookingConsumerServiceImpl.java
@@ -0,0 +1,378 @@
+package com.taken_seat.booking_service.booking.infrastructure.service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+import java.util.Random;
+import java.util.UUID;
+
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Caching;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.taken_seat.booking_service.booking.application.dto.command.BookingCreateCommand;
+import com.taken_seat.booking_service.booking.application.service.BookingClientService;
+import com.taken_seat.booking_service.booking.application.service.BookingCommandService;
+import com.taken_seat.booking_service.booking.application.service.BookingConsumerService;
+import com.taken_seat.booking_service.booking.application.service.BookingProducer;
+import com.taken_seat.booking_service.booking.domain.BenefitUsageHistory;
+import com.taken_seat.booking_service.booking.domain.Booking;
+import com.taken_seat.booking_service.booking.domain.BookingStatus;
+import com.taken_seat.booking_service.booking.domain.repository.BenefitUsageHistoryRepository;
+import com.taken_seat.booking_service.booking.domain.repository.BookingRepository;
+import com.taken_seat.booking_service.booking.presentation.dto.response.SeatLayoutResponseDto;
+import com.taken_seat.booking_service.common.message.TicketRequestMessage;
+import com.taken_seat.booking_service.common.service.RedisService;
+import com.taken_seat.common_service.dto.request.BookingSeatClientRequestDto;
+import com.taken_seat.common_service.dto.response.BookingSeatClientResponseDto;
+import com.taken_seat.common_service.exception.customException.BookingException;
+import com.taken_seat.common_service.exception.enums.ResponseCode;
+import com.taken_seat.common_service.message.BookingRequestMessage;
+import com.taken_seat.common_service.message.PaymentMessage;
+import com.taken_seat.common_service.message.PaymentRefundMessage;
+import com.taken_seat.common_service.message.UserBenefitMessage;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class BookingConsumerServiceImpl implements BookingConsumerService {
+
+	private final BenefitUsageHistoryRepository benefitUsageHistoryRepository;
+	private final BookingClientService bookingClientService;
+	private final BookingProducer bookingProducer;
+	private final BookingRepository bookingRepository;
+	private final RedisService redisService;
+	private final BookingCommandService bookingCommandService;
+
+	@Override
+	@Transactional
+	@Caching(evict = {
+		@CacheEvict(value = "readBooking", key = "#message.bookingId"),
+		@CacheEvict(value = "adminReadBooking", key = "#message.bookingId")
+	})
+	public void createPayment(UserBenefitMessage message) {
+
+		log.info(
+			"[Booking] 예매 쿠폰, 마일리지 적용 메시지 수신 - 시도: | userId={}, bookingId={}",
+			message.getUserId(),
+			message.getBookingId()
+		);
+
+		Booking booking = bookingRepository.findById(message.getBookingId())
+			.orElseThrow(() -> new BookingException(ResponseCode.BOOKING_NOT_FOUND_EXCEPTION));
+		int price = booking.getPrice(); // 정가
+
+		if (message.getStatus() == UserBenefitMessage.UserBenefitStatus.SUCCESS) {
+			if (message.getDiscount() != null) {
+				double discountAmount = price * (message.getDiscount() / 100.0);  // 할인 금액 계산
+				price = (int)(price - discountAmount); // 할인된 가격 계산
+
+				if (!isValidPrice(price)) {
+					log.warn(
+						"[Booking] 예매 쿠폰, 마일리지 적용 메시지 수신 - 실패: {} | userId={}, bookingId={}",
+						ResponseCode.INVALID_COUPON.getMessage(),
+						message.getUserId(),
+						message.getBookingId()
+					);
+					throw new BookingException(ResponseCode.INVALID_COUPON);
+				}
+			}
+
+			// 마일리지 차감
+			if (message.getMileage() != null) {
+				price -= message.getMileage();
+
+				if (!isValidPrice(price)) {
+					log.warn(
+						"[Booking] 예매 쿠폰, 마일리지 적용 메시지 수신 - 실패: {} | userId={}, bookingId={}",
+						ResponseCode.INVALID_MILEAGE.getMessage(),
+						message.getUserId(),
+						message.getBookingId()
+					);
+					throw new BookingException(ResponseCode.INVALID_MILEAGE);
+				}
+			}
+
+			// 쿠폰, 마일리지 사용내역 저장
+			BenefitUsageHistory history = BenefitUsageHistory.builder()
+				.bookingId(booking.getId())
+				.couponId(message.getCouponId())
+				.mileage(message.getMileage())
+				.usedAt(LocalDateTime.now())
+				.refunded(false)
+				.build();
+			history.prePersist(message.getUserId());
+
+			benefitUsageHistoryRepository.save(history);
+
+			// 결제 요청
+			PaymentMessage paymentMessage = PaymentMessage.builder()
+				.bookingId(booking.getId())
+				.userId(message.getUserId())
+				.price(booking.getDiscountedPrice())
+				.type(PaymentMessage.MessageType.REQUEST)
+				.build();
+
+			bookingProducer.sendPaymentRequest(paymentMessage);
+			log.info(
+				"[Booking] 예매 쿠폰, 마일리지 적용 메시지 수신 - 결제 요청 전송: | userId={}, bookingId={}",
+				message.getUserId(),
+				message.getBookingId()
+			);
+
+			booking.discount(price); // 할인가 업데이트
+
+			log.info(
+				"[Booking] 예매 쿠폰, 마일리지 적용 메시지 수신 - 성공: | userId={}, bookingId={}",
+				message.getUserId(),
+				message.getBookingId()
+			);
+
+			redisService.evictAllCaches("readBookings", message.getUserId().toString());
+			redisService.evictAllCaches("adminReadBookings", message.getUserId().toString());
+		} else {
+			log.warn(
+				"[Booking] 예매 쿠폰, 마일리지 적용 메시지 수신 - 실패: {} | userId={}, bookingId={}",
+				ResponseCode.BOOKING_BENEFIT_USAGE_FAILED_EXCEPTION.getMessage(),
+				message.getUserId(),
+				message.getBookingId()
+			);
+			throw new BookingException(ResponseCode.BOOKING_BENEFIT_USAGE_FAILED_EXCEPTION);
+		}
+	}
+
+	@Override
+	@Transactional
+	@Caching(evict = {
+		@CacheEvict(value = "readBooking", key = "#message.bookingId"),
+		@CacheEvict(value = "adminReadBooking", key = "#message.bookingId")
+	})
+	public void updateBooking(PaymentMessage message) {
+
+		log.info(
+			"[Booking] 예매 결제 메시지 수신 - 시도: | userId={}, bookingId={}",
+			message.getUserId(),
+			message.getBookingId()
+		);
+
+		Booking booking = bookingRepository.findById(message.getBookingId())
+			.orElseThrow(() -> new BookingException(ResponseCode.BOOKING_NOT_FOUND_EXCEPTION));
+		Optional<BenefitUsageHistory> optional = benefitUsageHistoryRepository.findByBookingIdAndRefundedIsFalse(
+			message.getBookingId());
+		PaymentMessage.PaymentResultStatus status = message.getStatus();
+
+		// 성공시
+		if (status == PaymentMessage.PaymentResultStatus.SUCCESS) {
+			Booking updated = booking.toBuilder()
+				.bookingStatus(BookingStatus.COMPLETED)
+				.paymentId(message.getPaymentId())
+				.bookedAt(LocalDateTime.now())
+				.build();
+			updated.preUpdate(message.getUserId());
+
+			bookingRepository.save(updated);
+
+			// 티켓 생성 요청
+			bookingProducer.sendTicketRequest(
+				TicketRequestMessage.builder()
+					.userId(booking.getUserId())
+					.bookingId(booking.getId())
+					.performanceId(booking.getPerformanceId())
+					.performanceScheduleId(booking.getPerformanceScheduleId())
+					.seatId(booking.getScheduleSeatId())
+					.build()
+			);
+
+			// 쿠폰, 마일리지 사용 내역 전달
+			if (optional.isPresent()) {
+				BenefitUsageHistory benefitUsageHistory = optional.get();
+				UserBenefitMessage benefitMessage = UserBenefitMessage.builder()
+					.bookingId(message.getBookingId())
+					.userId(message.getUserId())
+					.couponId(benefitUsageHistory.getCouponId())
+					.mileage(benefitUsageHistory.getMileage())
+					.price(booking.getPrice())
+					.status(UserBenefitMessage.UserBenefitStatus.SUCCESS)
+					.build();
+
+				bookingProducer.sendBenefitRefundRequest(benefitMessage);
+				log.info(
+					"[Booking] 예매 결제 메시지 수신 - 성공: 쿠폰, 마일리지 사용내역 전송 | userId={}, bookingId={}",
+					message.getUserId(),
+					message.getBookingId()
+				);
+			}
+
+			log.info(
+				"[Booking] 예매 결제 메시지 수신 - 성공: | userId={}, bookingId={}",
+				message.getUserId(),
+				message.getBookingId()
+			);
+
+			redisService.evictAllCaches("readBookings", message.getUserId().toString());
+			redisService.evictAllCaches("adminReadBookings", message.getUserId().toString());
+		} else {
+			// 실패시 사용한 쿠폰, 마일리지 원복처리
+
+			if (optional.isPresent()) {
+				BenefitUsageHistory benefitUsageHistory = optional.get();
+				benefitUsageHistory.refunded(message.getUserId());
+			}
+			log.warn(
+				"[Booking] 예매 결제 메시지 수신 - 실패: {} | userId={}, bookingId={}",
+				ResponseCode.BOOKING_PAYMENT_FAILED_EXCEPTION.getMessage(),
+				message.getUserId(),
+				message.getBookingId()
+			);
+
+			throw new BookingException(ResponseCode.BOOKING_PAYMENT_FAILED_EXCEPTION);
+		}
+	}
+
+	@Override
+	@Transactional
+	@Caching(evict = {
+		@CacheEvict(value = "readBooking", key = "#message.bookingId"),
+		@CacheEvict(value = "adminReadBooking", key = "#message.bookingId")
+	})
+	public void updateBooking(PaymentRefundMessage message) {
+
+		log.info(
+			"[Booking] 예매 환불 메시지 수신 - 시도: | userId={}, bookingId={}",
+			message.getUserId(),
+			message.getBookingId()
+		);
+
+		if (message.getStatus() == PaymentRefundMessage.PaymentRefundStatus.SUCCESS) {
+			Booking booking = bookingRepository.findById(message.getBookingId())
+				.orElseThrow(() -> new BookingException(ResponseCode.BOOKING_NOT_FOUND_EXCEPTION));
+			booking.cancel(message.getUserId());
+
+			Optional<BenefitUsageHistory> optional = benefitUsageHistoryRepository.findByBookingIdAndRefundedIsFalse(
+				booking.getId());
+
+			// 좌석 선점 취소 요청
+			BookingSeatClientRequestDto dto = new BookingSeatClientRequestDto(
+				booking.getPerformanceId(),
+				booking.getPerformanceScheduleId(),
+				booking.getScheduleSeatId()
+			);
+			BookingSeatClientResponseDto responseDto = bookingClientService.cancelSeatStatus(dto);
+
+			if (responseDto.reserved()) {
+				throw new BookingException(ResponseCode.BOOKING_SEAT_CANCEL_FAILED_EXCEPTION);
+			}
+
+			// 쿠폰, 마일리지 원복 요청
+			if (optional.isPresent()) {
+				BenefitUsageHistory history = optional.get();
+
+				UserBenefitMessage benefitMessage = UserBenefitMessage.builder()
+					.bookingId(booking.getId())
+					.userId(booking.getUserId())
+					.couponId(history.getCouponId())
+					.mileage(history.getMileage())
+					.price(booking.getPrice())
+					.status(UserBenefitMessage.UserBenefitStatus.REFUND)
+					.build();
+				bookingProducer.sendBenefitRefundRequest(benefitMessage);
+
+				log.info(
+					"[Booking] 예매 환불 메시지 수신 - 쿠폰, 마일리지 원복 요청 전송: | userId={}, bookingId={}",
+					message.getUserId(),
+					message.getBookingId()
+				);
+			}
+
+			log.info(
+				"[Booking] 예매 환불 메시지 수신 - 성공: | userId={}, bookingId={}",
+				message.getUserId(),
+				message.getBookingId()
+			);
+
+			redisService.evictAllCaches("readBookings", message.getUserId().toString());
+			redisService.evictAllCaches("adminReadBookings", message.getUserId().toString());
+		} else {
+			log.warn(
+				"[Booking] 예매 환불 메시지 수신 - 실패: {} | userId={}, bookingId={}",
+				ResponseCode.BOOKING_REFUND_FAILED_EXCEPTION.getMessage(),
+				message.getUserId(),
+				message.getBookingId()
+			);
+			throw new BookingException(ResponseCode.BOOKING_REFUND_FAILED_EXCEPTION);
+		}
+	}
+
+	@Override
+	@Transactional
+	public void updateBenefitUsageHistory(UserBenefitMessage message) {
+
+		log.info(
+			"[Booking] 예매 쿠폰, 마일리지 사용 내역 환불처리 - 시도: | userId={}, bookingId={}",
+			message.getUserId(),
+			message.getBookingId()
+		);
+
+		if (message.getStatus() == UserBenefitMessage.UserBenefitStatus.SUCCESS) {
+			BenefitUsageHistory history = benefitUsageHistoryRepository.findByBookingIdAndRefundedIsFalse(
+					message.getBookingId())
+				.orElseThrow(() -> new BookingException(ResponseCode.BOOKING_BENEFIT_USAGE_NOT_FOUND_EXCEPTION));
+
+			history.refunded(message.getUserId());
+
+			log.info(
+				"[Booking] 예매 쿠폰, 마일리지 사용 내역 환불처리 - 성공: | userId={}, bookingId={}",
+				message.getUserId(),
+				message.getBookingId()
+			);
+		} else {
+			log.warn(
+				"[Booking] 예매 쿠폰, 마일리지 사용 내역 환불처리 - 실패: {} | userId={}, bookingId={}",
+				ResponseCode.BOOKING_BENEFIT_USAGE_REFUND_FAILED_EXCEPTION.getMessage(),
+				message.getUserId(),
+				message.getBookingId()
+			);
+			throw new BookingException(ResponseCode.BOOKING_BENEFIT_USAGE_REFUND_FAILED_EXCEPTION);
+		}
+	}
+
+	@Override
+	@Transactional
+	public void acceptFromQueue(BookingRequestMessage message) {
+
+		log.info(
+			"[Booking] 대기열에서 입장: | userId={}, performanceId={}, performanceScheduleId={}",
+			message.getUserId(),
+			message.getPerformanceId(),
+			message.getPerformanceScheduleId()
+		);
+
+		SeatLayoutResponseDto layout = bookingClientService.getSeatLayout(message.getPerformanceScheduleId());
+		List<UUID> seatIds = layout.seats().stream()
+			.filter(e -> e.seatStatus().equals(SeatLayoutResponseDto.ScheduleSeatResponseDto.SeatStatus.AVAILABLE))
+			.map(SeatLayoutResponseDto.ScheduleSeatResponseDto::scheduleSeatId)
+			.toList();
+
+		Random random = new Random();
+		int i = random.nextInt(seatIds.size());
+		UUID seatId = seatIds.get(i);
+
+		BookingCreateCommand command = new BookingCreateCommand(
+			message.getUserId(),
+			"",
+			"",
+			message.getPerformanceId(),
+			message.getPerformanceScheduleId(), seatId
+		);
+
+		bookingCommandService.createBooking(command);
+	}
+
+	private boolean isValidPrice(int price) {
+		return price > 0;
+	}
+}
---
+++ com.taken_seat.booking-service/src/main/java/com/taken_seat/booking_service/booking/presentation/BookingCommandController.java
@@ -0,0 +1,77 @@
+package com.taken_seat.booking_service.booking.presentation;
+
+import java.util.UUID;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.taken_seat.booking_service.booking.application.dto.command.BookingCreateCommand;
+import com.taken_seat.booking_service.booking.application.dto.command.BookingPaymentCommand;
+import com.taken_seat.booking_service.booking.application.dto.command.BookingSingleTargetCommand;
+import com.taken_seat.booking_service.booking.application.dto.mapper.BookingMapper;
+import com.taken_seat.booking_service.booking.application.service.BookingCommandService;
+import com.taken_seat.booking_service.booking.presentation.dto.request.BookingCreateRequest;
+import com.taken_seat.booking_service.booking.presentation.dto.request.BookingPayRequest;
+import com.taken_seat.booking_service.booking.presentation.dto.response.BookingCreateResponse;
+import com.taken_seat.common_service.dto.ApiResponseData;
+import com.taken_seat.common_service.dto.AuthenticatedUser;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/bookings")
+public class BookingCommandController {
+
+	private final BookingCommandService bookingCommandService;
+	private final BookingMapper commandMapper;
+
+	@PostMapping
+	public ResponseEntity<ApiResponseData<BookingCreateResponse>> createBooking(AuthenticatedUser authenticatedUser,
+		@RequestBody @Valid BookingCreateRequest request) {
+
+		BookingCreateCommand command = commandMapper.toCommand(authenticatedUser, request);
+		BookingCreateResponse response = bookingCommandService.createBooking(command);
+
+		return ResponseEntity.status(HttpStatus.OK).body(ApiResponseData.success(response));
+	}
+
+	@PatchMapping("/{id}")
+	public ResponseEntity<ApiResponseData<Void>> cancelBooking(AuthenticatedUser authenticatedUser,
+		@PathVariable("id") UUID id) {
+
+		BookingSingleTargetCommand command = commandMapper.toCommand(authenticatedUser, id);
+		bookingCommandService.cancelBooking(command);
+
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).body(ApiResponseData.success());
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<ApiResponseData<Void>> deleteBooking(AuthenticatedUser authenticatedUser,
+		@PathVariable("id") UUID id) {
+
+		BookingSingleTargetCommand command = commandMapper.toCommand(authenticatedUser, id);
+		bookingCommandService.deleteBooking(command);
+
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).body(ApiResponseData.success());
+	}
+
+	@PostMapping("/{id}/payment")
+	public ResponseEntity<ApiResponseData<Void>> createPayment(AuthenticatedUser authenticatedUser,
+		@PathVariable("id") UUID id,
+		@RequestBody BookingPayRequest request) {
+
+		BookingPaymentCommand command = commandMapper.toCommand(authenticatedUser, id, request);
+		bookingCommandService.createPayment(command);
+
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).body(ApiResponseData.success());
+	}
+}
