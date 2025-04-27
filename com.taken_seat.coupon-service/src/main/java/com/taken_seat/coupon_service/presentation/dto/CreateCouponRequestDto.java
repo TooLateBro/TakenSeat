0c1a39dcfd616628476d8123de73b2144e022d21
SeungGyu <seungg8361@naver.com>
2025-04-27 23:45:22 +0900

    refactor : #414 <Coupon> Swagger 기능 수정
    - requestDto에 schema 추가
    - responseCode 추가

--- com.taken_seat.coupon-service/src/main/java/com/taken_seat/coupon_service/presentation/dto/CreateCouponRequestDto.java
+++ com.taken_seat.coupon-service/src/main/java/com/taken_seat/coupon_service/presentation/dto/CreateCouponRequestDto.java
@@ -2,6 +2,7 @@ package com.taken_seat.coupon_service.presentation.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.taken_seat.coupon_service.application.dto.CouponDto;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.Size;
 
@@ -9,18 +10,23 @@ import java.time.LocalDateTime;
 
 public record CreateCouponRequestDto(
         @Size(min = 1, max = 100, message = "쿠폰 이름은 1자 이상 100자 이하로 입력해주세요.")
-         String name,
+        @Schema(name = "CouponName")
+        String name,
 
         @Size(min = 1, max = 50, message = "코드는 1자 이상 50자 이하로 입력해주세요.")
+        @Schema(name = "CouponCode")
         String code,
 
         @Min(value = 1, message = "수량은 1 이상이어야 합니다.")
+        @Schema(name = "1")
         Long quantity,
 
         @Min(value = 1, message = "할인율은 1 이상이어야 합니다.")
+        @Schema(name = "1")
         Integer discount,
 
         @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+        @Schema(name = "2025-12-31 23:59:59")
         LocalDateTime expiredAt
 ) {
     public CouponDto toDto(){
