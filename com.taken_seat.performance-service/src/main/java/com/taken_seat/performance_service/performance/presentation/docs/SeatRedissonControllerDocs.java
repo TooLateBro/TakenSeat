8b328e6d8c7e214fc455e05e09504bec0e00f5c9
JUNG DAYE <jungyj0809@gmail.com>
2025-04-28 12:40:53 +0900

    docs: #419 공연 좌석 락 API Swagger 문서 작성
    - SeatRedissonControllerDocs 인터페이스 추가
      - 분산 락(Redisson)을 이용한 좌석 상태 변경 및 취소 API 명세 정의
    
    - SeatRedissonSwaggerDocs 커스텀 어노테이션 추가
      - Swagger 문서 커스터마이징
      - 요청 파라미터 설명 및 응답 코드 명시

--- com.taken_seat.performance-service/src/main/java/com/taken_seat/performance_service/performance/presentation/docs/SeatRedissonControllerDocs.java
+++ com.taken_seat.performance-service/src/main/java/com/taken_seat/performance_service/performance/presentation/docs/SeatRedissonControllerDocs.java
@@ -6,19 +6,19 @@ import org.springframework.web.bind.annotation.RequestBody;
 import com.taken_seat.common_service.dto.ApiResponseData;
 import com.taken_seat.common_service.dto.request.BookingSeatClientRequestDto;
 import com.taken_seat.common_service.dto.response.BookingSeatClientResponseDto;
+import com.taken_seat.performance_service.performance.infrastructure.swagger.SeatRedissonSwaggerDocs;
 
-import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @Tag(name = "공연 좌석 락 API", description = "Redisson 분산 락 기반 좌석 상태 변경 API입니다.")
 public interface SeatRedissonControllerDocs {
 
-	@Operation(summary = "락 기반 좌석 상태 변경", description = "좌석 예약 시 락을 걸어 상태를 변경합니다.")
+	@SeatRedissonSwaggerDocs.UpdateSeatStatusWithLock
 	ResponseEntity<ApiResponseData<BookingSeatClientResponseDto>> updateSeatStatusWithLock(
 		@RequestBody BookingSeatClientRequestDto request
 	);
 
-	@Operation(summary = "락 기반 좌석 상태 취소", description = "좌석 예약 취소 시 락을 걸어 상태를 되돌립니다.")
+	@SeatRedissonSwaggerDocs.UpdateSeatStatusCancelWithLock
 	ResponseEntity<ApiResponseData<BookingSeatClientResponseDto>> updateSeatStatusCancelWithLock(
 		@RequestBody BookingSeatClientRequestDto request
 	);
