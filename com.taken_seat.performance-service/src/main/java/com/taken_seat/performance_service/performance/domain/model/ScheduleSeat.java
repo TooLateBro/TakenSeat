ba07ea386a632c3cc5d2e28a66c9e54c3ea3e8b9
JUNG DAYE <jungyj0809@gmail.com>
2025-04-29 17:41:24 +0900

    feat: #436 ScheduleSeat 엔티티 updateStatus 메서드 제거
    - 좌석 상태 변경 책임을 Redis와 Kafka로 이관
    - ScheduleSeat의 updateStatus() 메서드 삭제

--- com.taken_seat.performance-service/src/main/java/com/taken_seat/performance_service/performance/domain/model/ScheduleSeat.java
+++ com.taken_seat.performance-service/src/main/java/com/taken_seat/performance_service/performance/domain/model/ScheduleSeat.java
@@ -3,8 +3,6 @@ package com.taken_seat.performance_service.performance.domain.model;
 import java.util.UUID;
 
 import com.taken_seat.common_service.entity.BaseTimeEntity;
-import com.taken_seat.common_service.exception.customException.PerformanceException;
-import com.taken_seat.common_service.exception.enums.ResponseCode;
 import com.taken_seat.performance_service.performance.application.dto.command.UpdateScheduleSeatCommand;
 import com.taken_seat.performance_service.performancehall.application.dto.command.SeatTemplateInfo;
 import com.taken_seat.performance_service.performancehall.domain.model.SeatStatus;
@@ -81,14 +79,5 @@ public class ScheduleSeat extends BaseTimeEntity {
 		seatType = command.seatType();
 		seatStatus = command.seatStatus();
 		price = command.price();
-	}
-
-	public void updateStatus(SeatStatus newStatus) {
-		if (this.seatStatus == SeatStatus.DISABLED) {
-			throw new PerformanceException(ResponseCode.SEAT_STATUS_CHANGE_NOT_ALLOWED);
-		}
-		this.seatStatus = newStatus;
-
-		performanceSchedule.updateStatusBasedOnSeats();
 	}
 }
