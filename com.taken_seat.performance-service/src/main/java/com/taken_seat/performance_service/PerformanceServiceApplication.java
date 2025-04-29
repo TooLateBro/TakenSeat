c515562729db9b0684c97b95b28b9c701190e4ee
JUNG DAYE <jungyj0809@gmail.com>
2025-04-30 00:04:17 +0900

    chore: @EnableScheduling 추가하여 스케줄러 기능 활성화

--- com.taken_seat.performance-service/src/main/java/com/taken_seat/performance_service/PerformanceServiceApplication.java
+++ com.taken_seat.performance-service/src/main/java/com/taken_seat/performance_service/PerformanceServiceApplication.java
@@ -3,12 +3,14 @@ package com.taken_seat.performance_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(scanBasePackages = {
 	"com.taken_seat.performance_service",
 	"com.taken_seat.common_service"
 })
 @EnableJpaAuditing
+@EnableScheduling
 public class PerformanceServiceApplication {
 
 	public static void main(String[] args) {
