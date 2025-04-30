6b5dca1e59d5e70c9b44b562abc1118a2dcbf58f
jjsh0208 <jjsh0208@gmail.com>
2025-05-01 02:24:46 +0900

    feat : #445 Payment 안쓰는 dto 제거

--- com.taken_seat.payment-service/src/main/java/com/taken_seat/payment_service/application/client/dto/CheckoutInfoResponse.java
+++
@@ -1,9 +0,0 @@
-package com.taken_seat.payment_service.application.client.dto;
-
-public record CheckoutInfoResponse(
-	String bookingId,
-	String orderName,
-	int originalAmount,
-	String customerKey
-) {
-}
