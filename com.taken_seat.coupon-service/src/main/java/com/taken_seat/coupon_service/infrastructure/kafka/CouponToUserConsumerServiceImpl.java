f3091b9d9af8e19cbdd6cdcddd9f605dad92cc12
SeungGyu <seungg8361@naver.com>
2025-05-03 00:37:58 +0900

    refactor : #464 <Coupon> 유저 중복로직 추가

--- com.taken_seat.coupon-service/src/main/java/com/taken_seat/coupon_service/infrastructure/kafka/CouponToUserConsumerServiceImpl.java
+++ com.taken_seat.coupon-service/src/main/java/com/taken_seat/coupon_service/infrastructure/kafka/CouponToUserConsumerServiceImpl.java
@@ -19,19 +19,24 @@ import java.time.Duration;
 public class CouponToUserConsumerServiceImpl implements CouponToUserConsumerService {
 
     private final CouponRepository couponRepository;
-    private final RedisTemplate<String, Long> redisTemplate;
+    private final RedisTemplate<String, Long> longRedisTemplate;
+    private final RedisTemplate<String, Object> objectRedisTemplate;
     private final RedissonClient redissonClient;
 
-    public CouponToUserConsumerServiceImpl(CouponRepository couponRepository, RedisTemplate<String, Long> redisTemplate, RedissonClient redissonClient) {
+    public CouponToUserConsumerServiceImpl(CouponRepository couponRepository,
+                                           RedisTemplate<String, Long> longRedisTemplate,
+                                           RedisTemplate<String, Object> objectRedisTemplate,
+                                           RedissonClient redissonClient) {
         this.couponRepository = couponRepository;
-        this.redisTemplate = redisTemplate;
+        this.longRedisTemplate = longRedisTemplate;
+        this.objectRedisTemplate = objectRedisTemplate;
         this.redissonClient = redissonClient;
     }
 
     @Transactional
     @Override
     public KafkaUserInfoMessage producerMessage(KafkaUserInfoMessage message){
-        String redisKey = "couponId:" + message.getCouponId();
+        String redisKey = "couponId : " + message.getCouponId();
 //        RLock lock = redissonClient.getLock(redisKey); // redis key 로 락 설정
         try {
 //            lock.tryLock(5, 2, TimeUnit.SECONDS); // 락 획득을 위한 최대 시간, 점유 최대 시간 설정 -> 5초, 2초
@@ -42,24 +47,31 @@ public class CouponToUserConsumerServiceImpl implements CouponToUserConsumerServ
                     .orElseThrow(() -> new CouponException(ResponseCode.COUPON_NOT_FOUND));
 
             // hasKey 로 키 존재 여부 확인 후 set 으로 저장
-            Boolean exists = redisTemplate.hasKey(redisKey); // 데이터 초기화
+            Boolean exists = longRedisTemplate.hasKey(redisKey); // 데이터 초기화
             if (exists == null || !exists) {
-                redisTemplate.opsForValue().set(redisKey, coupon.getQuantity(), Duration.ofSeconds(60));
+                longRedisTemplate.opsForValue().set(redisKey, coupon.getQuantity(), Duration.ofSeconds(60));
             }
 
-            Long currentQuantity = redisTemplate.opsForValue().get(redisKey);
+            Long currentQuantity = longRedisTemplate.opsForValue().get(redisKey);
 
             if (currentQuantity == null || currentQuantity == 0) {
                 throw new CouponException(ResponseCode.COUPON_QUANTITY_EXCEPTION);
             }
-            Long updatedQuantity = redisTemplate.opsForValue().decrement(redisKey);
+            Long updatedQuantity = longRedisTemplate.opsForValue().decrement(redisKey);
 
             if (updatedQuantity == 0 || updatedQuantity < 0) {
                 throw new CouponException(ResponseCode.COUPON_QUANTITY_EXCEPTION);
             }
+            String issuedUserKey = "issuedUsers : " + message.getCouponId();
+            Boolean existsUser = objectRedisTemplate.opsForSet().isMember(issuedUserKey, message.getUserId());
+            if (existsUser != null && existsUser) {
+                log.error("이미 발급에 성공한 유저입니다. userId : " + message.getUserId());
+                throw new IllegalArgumentException("중복된 userId 입니다.");
+            }
+            objectRedisTemplate.opsForSet().add(issuedUserKey, message.getUserId());
+            objectRedisTemplate.expire(issuedUserKey, Duration.ofMinutes(1));
 
             coupon.updateQuantity(updatedQuantity, message.getUserId(), coupon);
-//            redisTemplate.delete(redisKey);
             message.success(
                     message.getUserId(), message.getCouponId(), coupon.getDiscount(),
                     coupon.getExpiredAt(), KafkaUserInfoMessage.Status.SUCCEEDED
