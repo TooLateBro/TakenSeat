ab6d22b2232517a334069a13b98a775883e49586
jjsh0208 <jjsh0208@gmail.com>
2025-05-04 23:01:48 +0900

    feat : #466 Payment ProcessPaymentCommand 어노테이션 제거

--- com.taken_seat.payment-service/src/main/java/com/taken_seat/payment_service/application/command/event/ProcessPaymentCommand.java
+++ com.taken_seat.payment-service/src/main/java/com/taken_seat/payment_service/application/command/event/ProcessPaymentCommand.java
@@ -1,15 +1,9 @@
 package com.taken_seat.payment_service.application.command.event;
 
-import org.springframework.stereotype.Component;
-
 import com.taken_seat.common_service.message.PaymentMessage;
 import com.taken_seat.payment_service.application.command.PaymentCommand;
 import com.taken_seat.payment_service.application.service.event.PaymentEventHandlerService;
 
-import lombok.extern.slf4j.Slf4j;
-
-@Component
-@Slf4j
 public class ProcessPaymentCommand implements PaymentCommand<Void> {
 
 	private final PaymentEventHandlerService paymentEventHandlerService;
