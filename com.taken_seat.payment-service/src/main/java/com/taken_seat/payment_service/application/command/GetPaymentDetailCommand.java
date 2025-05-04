cfa9b23021ca1f10834d4fd8a399f24e6451a183
jjsh0208 <jjsh0208@gmail.com>
2025-05-04 21:47:01 +0900

    feat : #466 Payment command 디자인 패턴 적용
    - getPaymentDetail 기능 적용

---
+++ com.taken_seat.payment-service/src/main/java/com/taken_seat/payment_service/application/command/GetPaymentDetailCommand.java
@@ -0,0 +1,23 @@
+package com.taken_seat.payment_service.application.command;
+
+import java.util.UUID;
+
+import com.taken_seat.payment_service.application.dto.controller.response.PaymentDetailResDto;
+import com.taken_seat.payment_service.application.service.PaymentService;
+
+public class GetPaymentDetailCommand implements PaymentCommand<PaymentDetailResDto> {
+
+	private final PaymentService paymentService;
+
+	private UUID paymentId;
+
+	public GetPaymentDetailCommand(PaymentService paymentService, UUID paymentId) {
+		this.paymentService = paymentService;
+		this.paymentId = paymentId;
+	}
+
+	@Override
+	public PaymentDetailResDto execute() {
+		return paymentService.getPaymentDetail(paymentId);
+	}
+}
