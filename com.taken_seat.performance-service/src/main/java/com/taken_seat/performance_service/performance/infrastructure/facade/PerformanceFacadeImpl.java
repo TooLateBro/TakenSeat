a894b8bc7f64a7758efa52d554d21b1476b74ea3
JUNG DAYE <jungyj0809@gmail.com>
2025-05-05 21:34:43 +0900

    feat: #469 PerformanceFacadeImpl 메서드 구현
    - PerformanceRepository.findAllById 호출하여 RecommendedPerformanceDto 매핑 로직 작성

--- com.taken_seat.performance-service/src/main/java/com/taken_seat/performance_service/performance/infrastructure/facade/PerformanceFacadeImpl.java
+++ com.taken_seat.performance-service/src/main/java/com/taken_seat/performance_service/performance/infrastructure/facade/PerformanceFacadeImpl.java
@@ -1,12 +1,16 @@
 package com.taken_seat.performance_service.performance.infrastructure.facade;
 
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Component;
 
 import com.taken_seat.performance_service.performance.domain.facade.PerformanceFacade;
 import com.taken_seat.performance_service.performance.domain.model.Performance;
+import com.taken_seat.performance_service.performance.domain.repository.PerformanceRepository;
 import com.taken_seat.performance_service.performance.domain.validator.PerformanceExistenceValidator;
+import com.taken_seat.performance_service.performance.presentation.dto.request.RecommendedPerformanceDto;
 
 import lombok.RequiredArgsConstructor;
 
@@ -15,9 +19,21 @@ import lombok.RequiredArgsConstructor;
 public class PerformanceFacadeImpl implements PerformanceFacade {
 
 	private final PerformanceExistenceValidator performanceExistenceValidator;
+	private final PerformanceRepository performanceRepository;
 
 	@Override
 	public Performance getByPerformanceId(UUID performanceId) {
 		return performanceExistenceValidator.validateByPerformanceId(performanceId);
+	}
+
+	@Override
+	public List<RecommendedPerformanceDto> findRecommendedPerformancesByIds(List<UUID> ids) {
+		return performanceRepository.findAllById(ids).stream()
+			.map(entity -> new RecommendedPerformanceDto(
+				entity.getId(),
+				entity.getTitle(),
+				entity.getStartAt()
+			))
+			.collect(Collectors.toList());
 	}
 }
