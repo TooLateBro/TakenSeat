4c2cc8f0c7489ed32478a6f1ed1589ebda7341d6
JUNG DAYE <jungyj0809@gmail.com>
2025-05-06 21:32:49 +0900

    fix: #479 WebSecurityConfig 테스트용 API 보안 허용 경로 등록
    - /api/v1/users/test/snapshot, /api/v1/performances/test/recommend-requests 경로 permitAll() 처리

--- com.taken_seat.auth-service/src/main/java/com/taken_seat/auth_service/infrastructure/config/security/WebSecurityConfig.java
+++ com.taken_seat.auth-service/src/main/java/com/taken_seat/auth_service/infrastructure/config/security/WebSecurityConfig.java
@@ -13,37 +13,38 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class WebSecurityConfig {
 
-    private final String[] auth_Urls = {
-            "/api/v1/auths/**",
-            "/api/v1/users/**",
-            "/swagger-ui/**",
-            "v3/api-docs/**",
-            "/actuator/**"
-    };
+	private final String[] auth_Urls = {
+		"/api/v1/auths/**",
+		"/api/v1/users/test/snapshot",
+		"/api/v1/users/**",
+		"/swagger-ui/**",
+		"v3/api-docs/**",
+		"/actuator/**"
+	};
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        // CSRF 설정
-        http.csrf(AbstractHttpConfigurer::disable); // 람다식을 참조식으로 변경
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		// CSRF 설정
+		http.csrf(AbstractHttpConfigurer::disable); // 람다식을 참조식으로 변경
 
-        http.authorizeHttpRequests((authorizeHttpRequests) ->
-                authorizeHttpRequests
-                        .requestMatchers(auth_Urls).permitAll()
-                        .anyRequest().authenticated()
-        );
+		http.authorizeHttpRequests((authorizeHttpRequests) ->
+			authorizeHttpRequests
+				.requestMatchers(auth_Urls).permitAll()
+				.anyRequest().authenticated()
+		);
 
-        // 세션 사용 안함
-        http.sessionManagement(sessionManagement -> sessionManagement
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+		// 세션 사용 안함
+		http.sessionManagement(sessionManagement -> sessionManagement
+			.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
-        // security 로그인 기능 사용 안함
-        http.formLogin(AbstractHttpConfigurer::disable);
+		// security 로그인 기능 사용 안함
+		http.formLogin(AbstractHttpConfigurer::disable);
 
-        return http.build();
-    }
+		return http.build();
+	}
 
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
