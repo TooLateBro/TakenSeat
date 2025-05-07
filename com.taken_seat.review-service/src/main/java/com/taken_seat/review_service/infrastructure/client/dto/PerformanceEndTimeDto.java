5962140a6ac3fca53e97fbc12671eef0281d3f6a
jjsh0208 <jjsh0208@gmail.com>
2025-05-07 12:02:54 +0900

    refactor : #471 Review PerformanceEndTimeDto 직렬화 수정

--- com.taken_seat.review-service/src/main/java/com/taken_seat/review_service/infrastructure/client/dto/PerformanceEndTimeDto.java
+++ com.taken_seat.review-service/src/main/java/com/taken_seat/review_service/infrastructure/client/dto/PerformanceEndTimeDto.java
@@ -2,9 +2,6 @@ package com.taken_seat.review_service.infrastructure.client.dto;
 
 import java.time.LocalDateTime;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-
 public record PerformanceEndTimeDto(
-	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
 	LocalDateTime endAt) {
 }
