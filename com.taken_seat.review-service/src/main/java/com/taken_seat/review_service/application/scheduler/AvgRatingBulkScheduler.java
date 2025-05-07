d87bf3496e6159e5902c8301694c4fa172748f29
jjsh0208 <jjsh0208@gmail.com>
2025-05-07 21:35:28 +0900

    feat : #471 Review 호출 메서드 변경

--- com.taken_seat.review-service/src/main/java/com/taken_seat/review_service/application/scheduler/AvgRatingBulkScheduler.java
+++ com.taken_seat.review-service/src/main/java/com/taken_seat/review_service/application/scheduler/AvgRatingBulkScheduler.java
@@ -26,9 +26,10 @@ public class AvgRatingBulkScheduler {
 	})
 	public void fetchPerformanceRatingStatsBulk() {
 		log.info("[Review][Scheduler] 공연 별 평균 평점 계산 및 캐시 반영 시작");
+
 		try {
 			long start = System.currentTimeMillis();
-			redisRatingRepository.setAvgRatingBulk();
+			redisRatingRepository.setAvgRatingForChangedPerformances();
 			long end = System.currentTimeMillis();
 			System.out.println("평점 대량 저장 실행 시간: " + (end - start) + "ms");
 			log.info("[Review][Scheduler] 평균 평점 계산 및 캐시 반영 완료");
