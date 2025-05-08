688fb5f624a14ea88766969b8fa7c44e858ee8b6
SeungGyu <seungg8361@naver.com>
2025-05-08 20:05:17 +0900

    refactor : #490 <Gateway> 비동기 처리 로직 수정

--- com.taken_seat.gateway-service/src/main/java/com/taken_seat/gateway_service/config/RedisConfig.java
+++ com.taken_seat.gateway-service/src/main/java/com/taken_seat/gateway_service/config/RedisConfig.java
@@ -2,22 +2,26 @@ package com.taken_seat.gateway_service.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
 
     @Bean
-    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
-        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(redisConnectionFactory);
-        // key
-        redisTemplate.setKeySerializer(new StringRedisSerializer());
+    public ReactiveRedisTemplate<String, String> reactiveRedisTemplate(ReactiveRedisConnectionFactory redisConnectionFactory) {
+        // 직렬화 설정: 키와 값을 문자열로 처리
+        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+        RedisSerializationContext<String, String> serializationContext = RedisSerializationContext
+                .<String, String>newSerializationContext()
+                .key(stringRedisSerializer)
+                .value(stringRedisSerializer)
+                .hashKey(stringRedisSerializer)
+                .hashValue(stringRedisSerializer)
+                .build();
 
-        // value
-        redisTemplate.setValueSerializer(new StringRedisSerializer());
-        return redisTemplate;
+        return new ReactiveRedisTemplate<>(redisConnectionFactory, serializationContext);
     }
 }
