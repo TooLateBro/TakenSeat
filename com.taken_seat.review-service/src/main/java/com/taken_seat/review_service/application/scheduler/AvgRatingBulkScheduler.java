997ffd2c4d73b3051627bfc88214bcedc3cf66b2
jjsh0208 <jjsh0208@gmail.com>
2025-05-08 14:45:13 +0900

    feat : #471 Review 스케줄러 정상 변경

--- com.taken_seat.review-service/src/main/java/com/taken_seat/review_service/application/scheduler/AvgRatingBulkScheduler.java
+++ com.taken_seat.review-service/src/main/java/com/taken_seat/review_service/application/scheduler/AvgRatingBulkScheduler.java
@@ -18,8 +18,7 @@ public class AvgRatingBulkScheduler {
 	private final RedisRatingRepository redisRatingRepository;
 
 	// 한시간 마다 리뷰 평점을 업데이트
-	// @Scheduled(cron = "0 0 * * * ?")
-	@Scheduled(cron = "0 */2 * * * ?")
+	@Scheduled(cron = "0 0 * * * ?")
 	@Caching(evict = {
 		@CacheEvict(cacheNames = "reviewCache", allEntries = true),
 		@CacheEvict(cacheNames = "reviewSearchCache", allEntries = true)
@@ -28,10 +27,7 @@ public class AvgRatingBulkScheduler {
 		log.info("[Review][Scheduler] 공연 별 평균 평점 계산 및 캐시 반영 시작");
 
 		try {
-			long start = System.currentTimeMillis();
 			redisRatingRepository.setAvgRatingForChangedPerformances();
-			long end = System.currentTimeMillis();
-			System.out.println("평점 대량 저장 실행 시간: " + (end - start) + "ms");
 			log.info("[Review][Scheduler] 평균 평점 계산 및 캐시 반영 완료");
 		} catch (Exception e) {
 			log.error("[Review][Scheduler] 평균 평점 계산 중 예외 발생: {}", e.getMessage(), e);
